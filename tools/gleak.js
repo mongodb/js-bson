7758acc5c9c4994d8f0afe909884e91b37312527
Christian Kvalheim <christkv@gmail.com>
2012-03-09 16:08:57 +0100

    Refactored bson to work in systems where there is no typed arrays, added test

--- tools/gleak.js
+++ tools/gleak.js
@@ -3,5 +3,6 @@ var gleak = require('gleak')();
 gleak.ignore('AssertionError');
 gleak.ignore('testFullSpec_param_found');
 gleak.ignore('events');
+gleak.ignore('Uint8Array');
 
 module.exports = gleak;
