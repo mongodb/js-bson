a476efc6a46462a8420784a6e461ac9653683ade
Christian Kvalheim <christkv@gmail.com>
2012-09-25 15:35:29 +0200

    Added gleak excludes for windows build

--- tools/gleak.js
+++ tools/gleak.js
@@ -7,4 +7,13 @@ gleak.ignore('Uint8Array');
 gleak.ignore('Uint8ClampedArray');
 gleak.ignore('TAP_Global_Harness');
 
+gleak.ignore('DTRACE_NET_SERVER_CONNECTION');
+gleak.ignore('DTRACE_NET_STREAM_END');
+gleak.ignore('DTRACE_NET_SOCKET_READ');
+gleak.ignore('DTRACE_NET_SOCKET_WRITE');
+gleak.ignore('DTRACE_HTTP_SERVER_REQUEST');
+gleak.ignore('DTRACE_HTTP_SERVER_RESPONSE');
+gleak.ignore('DTRACE_HTTP_CLIENT_REQUEST');
+gleak.ignore('DTRACE_HTTP_CLIENT_RESPONSE');
+
 module.exports = gleak;
