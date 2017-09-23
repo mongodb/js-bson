1cdbdbd3f45a03494b889d9cf3b6fd7361116d3a
Matt Broadstone <mbroadst@gmail.com>
2017-09-23 13:29:51 -0400

    chore(mocha): convert all tests to use mocha and chai

--- test/node/map_tests.js
+++ test/node/map_tests.js
@@ -2,122 +2,125 @@
 
 var M = require('../../lib/bson/map');
 var createBSON = require('../utils');
+const expect = require('chai').expect;
 
-/**
- * @ignore
- */
-exports['should correctly exercise the map'] = function(test) {
-  var m = new M([['a', 1], ['b', 2]]);
-  test.ok(m.has('a'));
-  test.ok(m.has('b'));
-  test.equal(1, m.get('a'));
-  test.equal(2, m.get('b'));
-  test.ok(m.set('a', 3) === m);
-  test.ok(m.has('a'));
-  test.equal(3, m.get('a'));
+describe('Map', function() {
+  /**
+   * @ignore
+   */
+  it('should correctly exercise the map', function(done) {
+    var m = new M([['a', 1], ['b', 2]]);
+    expect(m.has('a')).to.be.ok;
+    expect(m.has('b')).to.be.ok;
+    expect(1).to.equal(m.get('a'));
+    expect(2).to.equal(m.get('b'));
+    expect(m.set('a', 3) === m).to.be.ok;
+    expect(m.has('a')).to.be.ok;
+    expect(3).to.equal(m.get('a'));
 
-  // Get the values
-  var iterator = m.values();
-  test.equal(3, iterator.next().value);
-  test.equal(2, iterator.next().value);
-  test.equal(true, iterator.next().done);
+    // Get the values
+    var iterator = m.values();
+    expect(3).to.equal(iterator.next().value);
+    expect(2).to.equal(iterator.next().value);
+    expect(true).to.equal(iterator.next().done);
 
-  // Get the entries
-  iterator = m.entries();
-  test.deepEqual(['a', 3], iterator.next().value);
-  test.deepEqual(['b', 2], iterator.next().value);
-  test.deepEqual(true, iterator.next().done);
+    // Get the entries
+    iterator = m.entries();
+    expect(['a', 3]).to.deep.equal(iterator.next().value);
+    expect(['b', 2]).to.deep.equal(iterator.next().value);
+    expect(true).to.deep.equal(iterator.next().done);
 
-  // Get the keys
-  iterator = m.keys();
-  test.deepEqual('a', iterator.next().value);
-  test.deepEqual('b', iterator.next().value);
-  test.deepEqual(true, iterator.next().done);
+    // Get the keys
+    iterator = m.keys();
+    expect('a').to.deep.equal(iterator.next().value);
+    expect('b').to.deep.equal(iterator.next().value);
+    expect(true).to.deep.equal(iterator.next().done);
 
-  // Collect values
-  var values = [];
-  // Get entries forEach
-  m.forEach(function(value, key, map) {
-    test.ok(value != null);
-    test.ok(key != null);
-    test.ok(map != null);
-    test.ok(m === this);
-    values.push([key, value]);
-  }, m);
+    // Collect values
+    var values = [];
+    // Get entries forEach
+    m.forEach(function(value, key, map) {
+      expect(value != null).to.be.ok;
+      expect(key != null).to.be.ok;
+      expect(map != null).to.be.ok;
+      expect(m === this).to.be.ok;
+      values.push([key, value]);
+    }, m);
 
-  test.deepEqual([['a', 3], ['b', 2]], values);
+    expect([['a', 3], ['b', 2]]).to.deep.equal(values);
 
-  // Modify the state
-  test.equal(true, m.delete('a'));
-  m.set('c', 5);
-  m.set('a', 7);
+    // Modify the state
+    expect(true).to.equal(m.delete('a'));
+    m.set('c', 5);
+    m.set('a', 7);
 
-  // Validate order is preserved
-  // Get the keys
-  iterator = m.keys();
-  test.deepEqual('b', iterator.next().value);
-  test.deepEqual('c', iterator.next().value);
-  test.deepEqual('a', iterator.next().value);
-  test.deepEqual(true, iterator.next().done);
+    // Validate order is preserved
+    // Get the keys
+    iterator = m.keys();
+    expect('b').to.deep.equal(iterator.next().value);
+    expect('c').to.deep.equal(iterator.next().value);
+    expect('a').to.deep.equal(iterator.next().value);
+    expect(true).to.deep.equal(iterator.next().done);
 
-  // Get the entries
-  iterator = m.entries();
-  test.deepEqual(['b', 2], iterator.next().value);
-  test.deepEqual(['c', 5], iterator.next().value);
-  test.deepEqual(['a', 7], iterator.next().value);
-  test.deepEqual(true, iterator.next().done);
+    // Get the entries
+    iterator = m.entries();
+    expect(['b', 2]).to.deep.equal(iterator.next().value);
+    expect(['c', 5]).to.deep.equal(iterator.next().value);
+    expect(['a', 7]).to.deep.equal(iterator.next().value);
+    expect(true).to.deep.equal(iterator.next().done);
 
-  // Get the values
-  iterator = m.values();
-  test.equal(2, iterator.next().value);
-  test.equal(5, iterator.next().value);
-  test.equal(7, iterator.next().value);
-  test.equal(true, iterator.next().done);
-  test.done();
-};
+    // Get the values
+    iterator = m.values();
+    expect(2).to.equal(iterator.next().value);
+    expect(5).to.equal(iterator.next().value);
+    expect(7).to.equal(iterator.next().value);
+    expect(true).to.equal(iterator.next().done);
+    done();
+  });
 
-/**
- * @ignore
- */
-exports['should serialize a map'] = function(test) {
-  // Serialize top level map only
-  var m = new M([['a', 1], ['b', 2]]);
-  var bson = createBSON();
-  // Serialize the map
-  var data = bson.serialize(m, false, true);
-  // Deserialize the data
-  var object = bson.deserialize(data);
-  test.deepEqual({ a: 1, b: 2 }, object);
+  /**
+   * @ignore
+   */
+  it('should serialize a map', function(done) {
+    // Serialize top level map only
+    var m = new M([['a', 1], ['b', 2]]);
+    var bson = createBSON();
+    // Serialize the map
+    var data = bson.serialize(m, false, true);
+    // Deserialize the data
+    var object = bson.deserialize(data);
+    expect({ a: 1, b: 2 }).to.deep.equal(object);
 
-  // Serialize nested maps
-  var m1 = new M([['a', 1], ['b', 2]]);
-  m = new M([['c', m1]]);
-  // Serialize the map
-  data = bson.serialize(m, false, true);
-  // Deserialize the data
-  object = bson.deserialize(data);
-  test.deepEqual({ c: { a: 1, b: 2 } }, object);
-  test.done();
+    // Serialize nested maps
+    var m1 = new M([['a', 1], ['b', 2]]);
+    m = new M([['c', m1]]);
+    // Serialize the map
+    data = bson.serialize(m, false, true);
+    // Deserialize the data
+    object = bson.deserialize(data);
+    expect({ c: { a: 1, b: 2 } }).to.deep.equal(object);
+    done();
 
-  // Serialize top level map only
-  m = new M([['1', 1], ['0', 2]]);
-  bson = createBSON();
-  // Serialize the map, validating that the order in the resulting BSON is preserved
-  data = bson.serialize(m, false, true);
-  test.equal('13000000103100010000001030000200000000', data.toString('hex'));
-};
+    // Serialize top level map only
+    m = new M([['1', 1], ['0', 2]]);
+    bson = createBSON();
+    // Serialize the map, validating that the order in the resulting BSON is preserved
+    data = bson.serialize(m, false, true);
+    expect('13000000103100010000001030000200000000').to.equal(data.toString('hex'));
+  });
 
-/**
- * @ignore
- */
-exports['should not crash due to object that looks like map'] = function(test) {
-  // Serialize top level map only
-  var m = { entries: 'test' };
-  var bson = createBSON();
-  // Serialize the map
-  var data = bson.serialize(m, false, true);
-  // Deserialize the data
-  var object = bson.deserialize(data);
-  test.deepEqual(m, object);
-  test.done();
-};
+  /**
+   * @ignore
+   */
+  it('should not crash due to object that looks like map', function(done) {
+    // Serialize top level map only
+    var m = { entries: 'test' };
+    var bson = createBSON();
+    // Serialize the map
+    var data = bson.serialize(m, false, true);
+    // Deserialize the data
+    var object = bson.deserialize(data);
+    expect(m).to.deep.equal(object);
+    done();
+  });
+});
