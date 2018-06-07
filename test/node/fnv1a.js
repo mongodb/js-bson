4f545b1e334895344d6163774a2aa3b368285eb5
Dan Aprahamian <dan.aprahamian@gmail.com>
2018-06-07 16:41:01 -0400

    feat(ObjectID): use FNV-1a hash for objectId
    Uses a hash of the hostname in the object id. Hashing algorithm
    is FNV-1a, 24-bit, using xor-folding.
    
    Fixes NODE-1484

---
+++ test/node/fnv1a.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const fnv1a = require('../../lib/bson/fnv1a');
+const fnv1a24 = fnv1a.fnv1a24;
+const expect = require('chai').expect;
+describe('fnv1a', function() {
+  require('./specs/object-id/vectors.json').vectors.forEach(testCase => {
+    const hash = testCase.hash;
+
+    let vector;
+    let encoding;
+    if (typeof testCase.vector === 'string') {
+      vector = testCase.vector;
+      encoding = 'utf8';
+    } else if (typeof testCase.vectorHex === 'string') {
+      vector = testCase.vectorHex;
+      encoding = 'hex';
+    }
+
+    it(`should properly hash the string "${vector}" with a 24 bit FNV-1a`, function() {
+      const hashed = fnv1a24(vector, encoding);
+      const buff = Buffer.from([(hashed >>> 16) & 0xff, (hashed >>> 8) & 0xff, hashed & 0xff]);
+      expect(buff.toString('hex')).to.equal(hash);
+    });
+  });
+});
