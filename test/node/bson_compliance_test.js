ced50a477af97782b2186e4c5443b0cc54b2d562
Sophie Saskin <sophie_saskin@brown.edu>
2018-06-29 17:44:12 -0400

    fix bson compliance tests

--- test/node/bson_compliance_test.js
+++ test/node/bson_compliance_test.js
@@ -9,7 +9,9 @@ var BSON = require('../..'),
   DBRef = BSON.DBRef,
   MinKey = BSON.MinKey,
   MaxKey = BSON.MaxKey,
-  expect = require('chai').expect;
+  expect = require('chai').expect,
+  scenariosCorrupt = require('./compliance/corrupt'),
+  scenariosValid = require('./compliance/valid');
 
 var createBSON = require('../utils');
 
@@ -18,14 +20,11 @@ describe('BSON Compliance', function() {
    * @ignore
    */
   it('Pass all corrupt BSON scenarios ./compliance/corrupt.json', function(done) {
-    // Read and parse the json file
-    var scenarios = require(__dirname + '/compliance/corrupt');
-
     // Create a new BSON instance
     var bson = createBSON();
 
-    for (var i = 0; i < scenarios.documents.length; i++) {
-      var doc = scenarios.documents[i];
+    for (var i = 0; i < scenariosCorrupt.documents.length; i++) {
+      var doc = scenariosCorrupt.documents[i];
       if (doc.skip) continue;
 
       try {
@@ -46,9 +45,6 @@ describe('BSON Compliance', function() {
    * @ignore
    */
   it('Pass all valid BSON serialization scenarios ./compliance/valid.json', function(done) {
-    // Read and parse the json file
-    var scenarios = require(__dirname + '/compliance/valid');
-
     // Create a new BSON instance
     var bson = createBSON();
 
@@ -99,7 +95,7 @@ describe('BSON Compliance', function() {
     };
 
     // Iterate over all the results
-    scenarios.documents.forEach(function(doc) {
+    scenariosValid.documents.forEach(function(doc) {
       if (doc.skip) return;
       // Create a buffer containing the payload
       var expectedData = new Buffer(doc.encoded, 'hex');
