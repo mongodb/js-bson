ad308df6852d31ec6e8cd56a5678f5c743e438cf
Sophie Saskin <sophie_saskin@brown.edu>
2018-07-03 10:36:43 -0400

    have test bash script work for running individual tests as well as both tests

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
-# run node tests
-node ./tools/loadTestFiles.js && mocha ./test/node
+ # run setup script
+node ./tools/loadTestFiles.js && 
 
-node_exit_code=$?
+# if no arguments
+if [ $# -eq 0 ]; then
 
-# if node exit status not ok, run cleanup and exit with code
-if [[ $node_exit_code != 0 ]] ; then node ./tools/deleteTestFiles.js && exit $node_exit_code ; fi
+    # run node test
+    mocha ./test/node
 
-# run karma tests
-./node_modules/karma/bin/karma start karma.conf.js
+    node_exit_code=$?
 
-karma_exit_code=$?
+    # if node exit status not ok, run cleanup and exit with code
+    if [[ $node_exit_code != 0 ]] ; then node ./tools/deleteTestFiles.js && exit $node_exit_code ; fi
+
+    # run karma tests
+    ./node_modules/karma/bin/karma start karma.conf.js
+
+elif [ $1 = "node" ]; then
+    # run node tests
+    mocha ./test/node
+
+else 
+     # run karma tests
+    ./node_modules/karma/bin/karma start karma.conf.js
+
+fi
+
+exit_code=$?
 
 # run cleanup script no matter what
 node ./tools/deleteTestFiles.js
 
 # exit with code if not ok
-if [[ $karma_exit_code != 0 ]] ; then exit $karma_exit_code ; fi
+if [[ $exit_code != 0 ]] ; then exit $exit_code ; fi
