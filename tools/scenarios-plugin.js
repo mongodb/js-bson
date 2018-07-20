a3fcc1935d7e23bc4ccc72f667c421d30a5abcdf
Sophie Saskin <sophie_saskin@brown.edu>
2018-07-20 11:10:21 -0400

    refactor(bson): minimize repeated code in testing
    Uses findScenarios function from tools/bson_corpus_loader.js instead of
    rewriting it in the scenariosPlugin.
    
    Fixes NODE-1504

--- tools/scenarios-plugin.js
+++ tools/scenarios-plugin.js
@@ -1,21 +1,13 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-
 const scenariosPlugin = () => {
   return {
     name: 'scenarios',
     load(id) {
       if (id.includes('bson_corpus_test_loader.js')) {
-        const corpus = fs
-          .readdirSync(path.join(__dirname, '../test/node/specs/bson-corpus'))
-          .filter(x => x.indexOf('json') !== -1)
-          .map(x =>
-            fs.readFileSync(path.join(__dirname, '../test/node/specs/bson-corpus', x), 'utf8')
-          );
+        const corpus = require('../test/node/tools/bson_corpus_test_loader');
         return {
-          code: `export default [ ${corpus.join(',')} ];\n`,
+          code: `export default ${JSON.stringify(corpus)};\n`,
           map: { mappings: '' }
         };
       }
