50de435875b45c3278394a0feea3c8e6c40ce6a2
Matt Broadstone <mbroadst@gmail.com>
2018-11-07 12:06:26 -0500

    test(bson): remove reference to `__filename`

--- test/node/bson_test.js
+++ test/node/bson_test.js
@@ -38,39 +38,40 @@ BSON.BSON_BINARY_SUBTYPE_USER_DEFINED = 128;
 /**
  * Module for parsing an ISO 8601 formatted string into a Date object.
  */
+const ISO_REGEX = /^(\d{4})(-(\d{2})(-(\d{2})(T(\d{2}):(\d{2})(:(\d{2})(\.(\d+))?)?(Z|((\+|-)(\d{2}):(\d{2}))))?)?)?$/;
 var ISODate = function(string) {
-  var match;
+  if (typeof string.getTime === 'function') {
+    return string;
+  }
 
-  if (typeof string.getTime === 'function') return string;
-  else if (
-    (match = string.match(
-      /^(\d{4})(-(\d{2})(-(\d{2})(T(\d{2}):(\d{2})(:(\d{2})(\.(\d+))?)?(Z|((\+|-)(\d{2}):(\d{2}))))?)?)?$/
-    ))
-  ) {
-    var date = new Date();
-    date.setUTCFullYear(Number(match[1]));
-    date.setUTCMonth(Number(match[3]) - 1 || 0);
-    date.setUTCDate(Number(match[5]) || 0);
-    date.setUTCHours(Number(match[7]) || 0);
-    date.setUTCMinutes(Number(match[8]) || 0);
-    date.setUTCSeconds(Number(match[10]) || 0);
-    date.setUTCMilliseconds(Number('.' + match[12]) * 1000 || 0);
+  const match = string.match(ISO_REGEX);
+  if (!match) {
+    throw new Error(`Invalid ISO 8601 date given: ${string}`);
+  }
 
-    if (match[13] && match[13] !== 'Z') {
-      var h = Number(match[16]) || 0,
-        m = Number(match[17]) || 0;
+  var date = new Date();
+  date.setUTCFullYear(Number(match[1]));
+  date.setUTCMonth(Number(match[3]) - 1 || 0);
+  date.setUTCDate(Number(match[5]) || 0);
+  date.setUTCHours(Number(match[7]) || 0);
+  date.setUTCMinutes(Number(match[8]) || 0);
+  date.setUTCSeconds(Number(match[10]) || 0);
+  date.setUTCMilliseconds(Number('.' + match[12]) * 1000 || 0);
 
-      h *= 3600000;
-      m *= 60000;
+  if (match[13] && match[13] !== 'Z') {
+    var h = Number(match[16]) || 0,
+      m = Number(match[17]) || 0;
 
-      var offset = h + m;
-      if (match[15] === '+') offset = -offset;
+    h *= 3600000;
+    m *= 60000;
 
-      date = new Date(date.valueOf() + offset);
-    }
+    var offset = h + m;
+    if (match[15] === '+') offset = -offset;
 
-    return date;
-  } else throw new Error('Invalid ISO 8601 date given.', __filename);
+    date = new Date(date.valueOf() + offset);
+  }
+
+  return date;
 };
 
 describe('BSON', function() {
