e7067e059c52e4b914d1426f02a7440351f4dc76
Dan Aprahamian <dan.aprahamian@gmail.com>
2019-04-17 15:05:16 -0400

    fix(Timestamp): make sure timestamp is always unsigned
    Fixes NODE-1939

---
+++ test/node/timestamp_tests.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const BSON = require('../../lib/bson');
+const expect = require('chai').expect;
+
+describe('Timestamp', function() {
+  it('should have a MAX_VALUE equal to Long.MAX_UNSIGNED_VALUE', function() {
+    expect(BSON.Timestamp.MAX_VALUE).to.equal(BSON.Long.MAX_UNSIGNED_VALUE);
+  });
+
+  it('should always be an unsigned value', function() {
+    [
+      new BSON.Timestamp(),
+      new BSON.Timestamp(0xff, 0xffffffff),
+      new BSON.Timestamp(0xffffffff, 0xffffffff),
+      new BSON.Timestamp(-1, -1),
+      new BSON.Timestamp(new BSON.Timestamp(0xffffffff, 0xffffffff)),
+      new BSON.Timestamp(new BSON.Long(0xffffffff, 0xfffffffff, false)),
+      new BSON.Timestamp(new BSON.Long(0xffffffff, 0xfffffffff, true))
+    ].forEach(timestamp => {
+      expect(timestamp).to.have.property('unsigned', true);
+    });
+  });
+
+  it('should print out an unsigned number', function() {
+    const timestamp = new BSON.Timestamp(0xffffffff, 0xffffffff);
+    expect(timestamp.toString()).to.equal('18446744073709551615');
+    expect(timestamp.toJSON()).to.deep.equal({ $timestamp: '18446744073709551615' });
+    expect(timestamp.toExtendedJSON()).to.deep.equal({
+      $timestamp: { t: 4294967295, i: 4294967295 }
+    });
+  });
+});
