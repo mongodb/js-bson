cb2d70d755f73eab94150503857fa4eaa30dd0bf
Chua Kang Ming <kambing860210@gmail.com>
2020-01-14 16:13:13 +0800

    fix test

--- test/node/binary_test.js
+++ test/node/binary_test.js
@@ -9,7 +9,7 @@ describe('Binary', function() {
     /* eslint-disable */
     const randomBuffer = crypto.randomBytes
       ? crypto.randomBytes(256)
-      : crypto.getRandomValues(new Uint32Array(256));
+      : window.crypto.getRandomValues(new Uint32Array(256));
     /* eslint-enable */
     const binary1 = new Binary(randomBuffer);
     const binary2 = new Binary(binary1.value(), binary1.sub_type);
