83f9da8293ea41d93fe0a44b7d94d4bee4812297
Chua Kang Ming <kambing860210@gmail.com>
2020-01-14 16:08:36 +0800

    fix test

--- test/node/binary_test.js
+++ test/node/binary_test.js
@@ -4,9 +4,13 @@ const crypto = require('crypto');
 const expect = require('chai').expect;
 const Binary = require('../../lib/binary');
 
-describe.only('Binary', function() {
+describe('Binary', function() {
   it('Should convert Binary to itself with same value', function(done) {
-    const randomBuffer = crypto.randomBytes(256);
+    /* eslint-disable */
+    const randomBuffer = crypto.randomBytes
+      ? crypto.randomBytes(256)
+      : crypto.getRandomValues(new Uint32Array(256));
+    /* eslint-enable */
     const binary1 = new Binary(randomBuffer);
     const binary2 = new Binary(binary1.value(), binary1.sub_type);
     expect(binary1.value(), 'both value() should return same value').to.equal(binary2.value());
