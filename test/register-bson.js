1b0fea99c773a774a614fbe996bcdf5b81d7a34e
Mark Vasilkov <MarkV@nsogroup.com>
2020-09-17 16:58:04 +0300

    chore: remove the unused FNV-1a hash function

--- test/register-bson.js
+++ test/register-bson.js
@@ -11,10 +11,7 @@ require('chai/register-expect');
 
 const BSON = require('../lib/bson');
 const { ensureBuffer } = require('../lib/ensure_buffer');
-const { fnv1a24, fnv1a32 } = require('../lib/fnv1a');
 
 BSON.ensureBuffer = ensureBuffer;
-BSON.fnv1a24 = fnv1a24;
-BSON.fnv1a32 = fnv1a32;
 
 module.exports = BSON;
