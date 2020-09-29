53cb204f67da5dbadfdbe1380832ec5b6ace2548
Neal Beeken <neal.beeken@mongodb.com>
2020-09-29 10:21:22 -0400

    refactor: :fire: Remove rollup specific TSConfig and inline it into the rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,32 @@ import typescript from '@rollup/plugin-typescript';
 // Entry point of the library
 const input = './src/bson.ts';
 
+const tsConfig = {
+  allowJs: false,
+  checkJs: false,
+  strict: true,
+  alwaysStrict: true,
+  target: 'ES2017',
+  module: 'esnext',
+  moduleResolution: 'node',
+  lib: ['ES2017', 'ES2020.BigInt', 'ES2017.TypedArrays'],
+  // We don't make use of tslib helpers
+  importHelpers: true,
+  noEmitHelpers: false,
+  noEmitOnError: true,
+  // make use of import type where applicable
+  importsNotUsedAsValues: 'error',
+  // Generate separate source maps files with sourceContent included
+  sourceMap: true,
+  inlineSourceMap: false,
+  inlineSources: false,
+  // API-extractor makes use of the declarations, npm script should be cleaning these up
+  declaration: false,
+  types: [],
+  tsconfig: false,
+  include: ['src/**/*']
+};
+
 export default [
   {
     input,
@@ -18,7 +44,7 @@ export default [
       sourcemap: true
     },
     plugins: [
-      typescript({ tsconfig: './tsconfig.rollup.json' }),
+      typescript(tsConfig),
       nodeResolve({ preferBuiltins: false }),
       commonjs(),
       nodePolyfills(),
@@ -53,7 +79,7 @@ export default [
       }
     ],
     plugins: [
-      typescript({ tsconfig: './tsconfig.rollup.json' }),
+      typescript(tsConfig),
       nodeResolve({ preferBuiltins: false }),
       commonjs(),
       nodePolyfills(),
@@ -84,13 +110,7 @@ export default [
         sourcemap: true
       }
     ],
-    // Notice the external buffer, and preferBuiltins false,
-    // this bundle doesn't pull in 'buffer' or Map from 'core-js'
-    external: ['buffer'],
-    plugins: [
-      typescript({ tsconfig: './tsconfig.rollup.json' }),
-      nodeResolve({ preferBuiltins: true }),
-      commonjs()
-    ]
+    external: ['buffer'], // don't bundle 'buffer'
+    plugins: [typescript(tsConfig), nodeResolve({ preferBuiltins: true }), commonjs()]
   }
 ];
