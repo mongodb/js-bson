213468ea6db58e571e930d43d4742eae9f765cc1
Thomas Reggi <thomas.reggi@mongodb.com>
2020-09-30 15:20:19 -0400

    attribution

--- src/uuid.ts
+++ src/uuid.ts
@@ -1,3 +1,7 @@
+/**
+ * UUID regular expression pattern copied from `uuid` npm module.
+ * @see https://github.com/uuidjs/uuid/blob/master/src/regex.js
+ */
 const UUID_RX = /^(?:[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000)$/i;
 
 export interface UUIDExtended {
