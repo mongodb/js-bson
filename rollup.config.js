a48676b0d442e06a71a413500194d35a7bea7587
Neal Beeken <neal.beeken@mongodb.com>
2020-09-30 09:14:32 -0400

    fix: Rework rollup config to output named and default exports (#404)
    Bundles now fixed to support both named and default exports
    as they previously had.
    
    NODE-2712

--- rollup.config.js
+++ rollup.config.js
@@ -1,61 +1,91 @@
-import babel from '@rollup/plugin-babel';
-const commonjs = require('@rollup/plugin-commonjs');
+const pkg = require('./package.json');
+const commonjs = require('rollup-plugin-commonjs');
+const nodeBuiltins = require('rollup-plugin-node-builtins');
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
-const nodePolyfills = require('rollup-plugin-node-polyfills');
+const { babel } = require('@rollup/plugin-babel');
+const typescript = require('@rollup/plugin-typescript');
 
-// Entry point of the library
-const input = './lib/bson.js';
+const tsConfig = {
+  allowJs: false,
+  checkJs: false,
+  strict: true,
+  alwaysStrict: true,
+  target: 'ES2017',
+  module: 'commonjs',
+  moduleResolution: 'node',
+  lib: ['ES2017', 'ES2020.BigInt', 'ES2017.TypedArrays'],
+  // We don't make use of tslib helpers
+  importHelpers: true,
+  noEmitHelpers: false,
+  noEmitOnError: true,
+  // make use of import type where applicable
+  importsNotUsedAsValues: 'error',
+  // Generate separate source maps files with sourceContent included
+  sourceMap: true,
+  inlineSourceMap: false,
+  inlineSources: false,
+  // API-extractor makes use of the declarations, npm script should be cleaning these up
+  declaration: false,
+  types: [],
+  tsconfig: false,
+  include: ['src/**/*']
+};
+const input = 'src/bson.ts';
 
-export default [
-  {
-    input,
-    output: [
-      /* Browser ESM bundle */
-      {
-        file: 'dist/bson.browser.esm.js',
-        format: 'es'
-      },
-      /* Browser UMD bundle */
-      {
-        file: 'dist/bson.browser.umd.js',
-        format: 'umd',
-        name: 'BSON'
-      },
-      /* Browser IIFE bundle */
-      {
-        file: 'dist/bson.bundle.js',
-        format: 'iife',
-        name: 'BSON'
-      }
-    ],
+const plugins = [
+  typescript(tsConfig),
+  nodeResolve({ preferBuiltins: false }),
+  commonjs({ extensions: ['.js', '.ts'] }),
+  nodeBuiltins(),
+  babel({
+    babelHelpers: 'external',
+    plugins: ['@babel/plugin-external-helpers'],
+    presets: [['@babel/env', { modules: false }]]
+  })
+];
 
-    plugins: [
-      nodeResolve({ preferBuiltins: false }),
-      commonjs(),
-      nodePolyfills({ include: ['buffer'] }),
-      babel({
-        babelHelpers: 'bundled',
-        presets: [
-          [
-            '@babel/env',
-            {
-              modules: false
-            }
-          ]
-        ]
-      })
-    ]
-  },
-  /* ESM bundle with externals */
+const external = Object.keys(pkg.dependencies || {});
+
+const defaultName = 'BSON';
+
+module.exports = [
   {
     input,
     output: {
       file: 'dist/bson.esm.js',
-      format: 'es'
+      format: 'es',
+      name: defaultName,
+      exports: 'named',
+      sourcemap: true
     },
-    // Notice the external buffer, and preferBuiltins false,
-    // this bundle doesn't pull in 'buffer' or Map from 'core-js'
-    external: ['buffer'],
-    plugins: [nodeResolve({ preferBuiltins: true }), commonjs()]
+    plugins,
+    external
+  },
+  {
+    input,
+    output: [
+      {
+        file: 'dist/bson.browser.umd.js',
+        format: 'umd',
+        name: defaultName,
+        exports: 'named',
+        sourcemap: true
+      },
+      {
+        file: 'dist/bson.browser.esm.js',
+        format: 'es',
+        name: defaultName,
+        exports: 'named',
+        sourcemap: true
+      },
+      {
+        file: 'dist/bson.bundle.js',
+        format: 'iife',
+        name: defaultName,
+        exports: 'named',
+        sourcemap: true
+      }
+    ],
+    plugins
   }
 ];
