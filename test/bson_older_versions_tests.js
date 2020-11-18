f4e6c691550153e4846aed95c25df2f7d1a65ceb
Neal Beeken <neal.beeken@mongodb.com>
2020-11-18 12:24:46 -0500

    Add docs to older versions tests

--- test/bson_older_versions_tests.js
+++ test/bson_older_versions_tests.js
@@ -6,11 +6,23 @@ const fetch = require('node-fetch').default;
 const rimraf = require('rimraf');
 const cp = require('child_process');
 
+/*
+ * This file tests that previous versions of BSON
+ * serialize and deserialize correctly in the most recent version of BSON
+ *
+ * This is an unusual situation to run into as users should be using one BSON lib version
+ * but it does arise with sub deps etc. and we wish to protect against unexpected behavior
+ *
+ * If backwards compatibility breaks there should be clear warnings/failures
+ * rather than empty or zero-ed values.
+ */
+
 const OLD_VERSIONS = ['v1.1.5', 'v1.1.4'];
 const getZipUrl = ver => `https://github.com/mongodb/js-bson/archive/${ver}.zip`;
 const getImportPath = ver => `../bson-${ver}/js-bson-${ver.substring(1)}`;
 
 function downloadZip(version, done) {
+  // downloads a zip of previous BSON version
   fetch(getZipUrl(version))
     .then(r => {
       return r.arrayBuffer();
@@ -18,6 +30,7 @@ function downloadZip(version, done) {
     .then(r => {
       fs.writeFileSync(`bson-${version}.zip`, new Uint8Array(r));
       try {
+        // unzips the code, right now these test won't handle versions written in TS
         cp.execSync(`unzip bson-${version}.zip -d bson-${version}`);
       } catch (err) {
         return done(err);
@@ -30,6 +43,8 @@ describe('Current version', function () {
   OLD_VERSIONS.forEach(version => {
     before(function (done) {
       if (Number(process.version.split('.')[0].substring(1)) < 8) {
+        // WHATWG fetch doesn't download correctly prior to node 8
+        // but we should be safe by testing on node 8 +
         this.skip();
       }
       if (fs.existsSync(`bson-${version}.zip`)) {
