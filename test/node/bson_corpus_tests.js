8b85a708d22f9f218bcdcf2c9c289544a9605045
Durran Jordan <durran@gmail.com>
2021-05-12 20:38:23 +0200

    test: bson corpus spec runner calling wrong fn

--- test/node/bson_corpus_tests.js
+++ test/node/bson_corpus_tests.js
@@ -192,7 +192,7 @@ describe('BSON Corpus', function () {
         describe('parseErrors', function () {
           scenario.parseErrors.forEach(p => {
             it(p.description, function () {
-              expect(() => Decimal128.fromString(scenario.string)).to.throw();
+              expect(() => jsonToNative(scenario.string)).to.throw();
             });
           });
         });
