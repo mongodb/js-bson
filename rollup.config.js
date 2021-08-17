b64a8437b8993be1f8c22b7c80697bab28df95c4
Neal Beeken <neal.beeken@mongodb.com>
2021-08-17 16:17:24 -0400

    fix: depedabot warnings

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import pkg from './package.json';
 import commonjs from 'rollup-plugin-commonjs';
-import nodeBuiltins from 'rollup-plugin-node-builtins';
+import nodeBuiltins from 'rollup-plugin-polyfill-node';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { babel } from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
