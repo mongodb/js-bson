da8d8f1c0a9eaff7df6468a77e98282b8456cc3a
Neal Beeken <neal.beeken@mongodb.com>
2021-09-20 17:34:07 -0400

    test(NODE-3617): serialization of BSON with embedded null bytes in strings (#463)

---
+++ test/node/null_byte_test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const expect = require('chai').expect;
+const BSONLib = require('../../index');
+const createBSON = require('../utils');
+const BSON = Object.assign({}, BSONLib, Object.getPrototypeOf(createBSON()));
+
+const tests = new Map();
+const it = (title, testFn) => tests.set(title, (test) => {
+    testFn();
+    test.done();
+});
+
+it('null byte handling in serializing should throw when null byte in BSON Field name within a root document', () => {
+    expect(() => BSON.serialize({ 'a\x00b': 1 })).to.throw(/null bytes/);
+});
+
+it('null byte handling in serializing should throw when null byte in BSON Field name within a sub-document', () => {
+    expect(() => BSON.serialize({ a: { 'a\x00b': 1 } })).to.throw(/null bytes/);
+});
+
+it('null byte handling in serializing should throw when null byte in Pattern for a regular expression', () => {
+    // eslint-disable-next-line no-control-regex
+    expect(() => BSON.serialize({ a: new RegExp('a\x00b') })).to.throw(/null bytes/);
+    expect(() => BSON.serialize({ a: new BSON.BSONRegExp('a\x00b') })).to.throw(/null bytes/);
+});
+
+it('null byte handling in serializing should throw when null byte in Flags/options for a regular expression', () => {
+    expect(() => BSON.serialize({ a: new BSON.BSONRegExp('a', 'i\x00m') })).to.throw(/regular expression option/);
+});
+
+module.exports = {};
+for (const entry of tests) {
+    module.exports[entry[0]] = entry[1];
+}
