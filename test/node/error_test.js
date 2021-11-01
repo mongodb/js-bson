ad2c4a5816bbd29d0f618d26d9ece4c8802e04ef
Grace Chong <grace.chong@mongodb.com>
2021-11-01 16:54:01 -0400

    refactor: remove assertion override and add tests to check for correct error message and names

---
+++ test/node/error_test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const BSON = require('../register-bson');
+const BSONTypeError = BSON.BSONTypeError;
+const BSONError = BSON.BSONError;
+
+describe('BSON error tests', function () {
+  it('should correctly validate instanceof checks for BSONTypeError and BSONError', function () {
+    const bsonTypeErr = new BSONTypeError();
+    const bsonErr = new BSONError();
+
+    expect(bsonTypeErr instanceof BSONTypeError).to.be.true;
+    expect(bsonErr instanceof BSONError).to.be.true;
+  });
+
+  it('should correctly get correct names for BSONTypeError and BSONError', function () {
+    const bsonTypeErr = new BSONTypeError('This is a BSONTypeError message');
+    const bsonErr = new BSONError('This is a BSONError message');
+
+    expect(bsonTypeErr.name).equals('BSONTypeError');
+    expect(bsonTypeErr.message).equals('This is a BSONTypeError message');
+    expect(bsonErr.name).equals('BSONError');
+    expect(bsonErr.message).equals('This is a BSONError message');
+  });
+});
