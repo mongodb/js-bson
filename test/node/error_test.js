d6d0732fb67fe4bf51a7ab0cd80cd89523414a02
Grace Chong <grace.chong@mongodb.com>
2021-11-02 17:25:17 -0400

    refactor: split up test cases for clarity and restructure

--- test/node/error_test.js
+++ test/node/error_test.js
@@ -4,23 +4,31 @@ const BSON = require('../register-bson');
 const BSONTypeError = BSON.BSONTypeError;
 const BSONError = BSON.BSONError;
 
-describe('BSON error tests', function () {
-  it('should correctly validate instanceof checks for BSONTypeError and BSONError', function () {
+describe('BSONTypeError', function () {
+  it('should evaluate true on instanceof BSONTypeError and TypeError', function () {
     const bsonTypeErr = new BSONTypeError();
-    const bsonErr = new BSONError();
-
+    expect(bsonTypeErr instanceof BSONTypeError).to.be.true;
     expect(bsonTypeErr).to.be.instanceOf(BSONTypeError);
     expect(bsonTypeErr).to.be.instanceOf(TypeError);
+  });
+
+  it('should correctly set BSONTypeError name and message properties', function () {
+    const bsonTypeErr = new BSONTypeError('This is a BSONTypeError message');
+    expect(bsonTypeErr.name).equals('BSONTypeError');
+    expect(bsonTypeErr.message).equals('This is a BSONTypeError message');
+  });
+});
+
+describe('BSONError', function () {
+  it('should evaluate true on instanceof BSONError and Error', function () {
+    const bsonErr = new BSONError();
+    expect(bsonErr instanceof BSONError).to.be.true;
     expect(bsonErr).to.be.instanceOf(BSONError);
     expect(bsonErr).to.be.instanceOf(Error);
   });
 
-  it('should correctly get correct names for BSONTypeError and BSONError', function () {
-    const bsonTypeErr = new BSONTypeError('This is a BSONTypeError message');
+  it('should correctly set BSONError name and message properties', function () {
     const bsonErr = new BSONError('This is a BSONError message');
-
-    expect(bsonTypeErr.name).equals('BSONTypeError');
-    expect(bsonTypeErr.message).equals('This is a BSONTypeError message');
     expect(bsonErr.name).equals('BSONError');
     expect(bsonErr.message).equals('This is a BSONError message');
   });
