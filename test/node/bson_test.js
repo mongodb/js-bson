6150a8d492c89733d05771804a2514fe306d1262
Daria Pardue <daria.pardue@mongodb.com>
2022-02-09 17:59:01 -0500

    test(NODE-3719): reorganize spec tests for visibility

--- test/node/bson_test.js
+++ test/node/bson_test.js
@@ -1986,38 +1986,4 @@ describe('BSON', function () {
       expect(inspect(timestamp)).to.equal('new Timestamp({ t: 100, i: 1 })');
     });
   });
-
-  /**
-   * The BSON spec uses null-terminated strings to represent document field names and
-   * regex components (i.e. pattern and flags/options). Drivers MUST assert that null
-   * bytes are prohibited in the following contexts when encoding BSON (i.e. creating
-   * raw BSON bytes or constructing BSON-specific type classes):
-   * - Field name within a root document
-   * - Field name within a sub-document
-   * - Pattern for a regular expression
-   * - Flags/options for a regular expression
-   * Depending on how drivers implement BSON encoding, they MAY expect an error when
-   * constructing a type class (e.g. BSON Document or Regex class) or when encoding a
-   * language representation to BSON (e.g. converting a dictionary, which might allow
-   * null bytes in its keys, to raw BSON bytes).
-   */
-  describe('null byte handling during serializing', () => {
-    it('should throw when null byte in BSON Field name within a root document', () => {
-      expect(() => BSON.serialize({ 'a\x00b': 1 })).to.throw(/null bytes/);
-    });
-
-    it('should throw when null byte in BSON Field name within a sub-document', () => {
-      expect(() => BSON.serialize({ a: { 'a\x00b': 1 } })).to.throw(/null bytes/);
-    });
-
-    it('should throw when null byte in Pattern for a regular expression', () => {
-      // eslint-disable-next-line no-control-regex
-      expect(() => BSON.serialize({ a: new RegExp('a\x00b') })).to.throw(/null bytes/);
-      expect(() => BSON.serialize({ a: new BSONRegExp('a\x00b') })).to.throw(/null bytes/);
-    });
-
-    it('should throw when null byte in Flags/options for a regular expression', () => {
-      expect(() => BSON.serialize({ a: new BSONRegExp('a', 'i\x00m') })).to.throw(/null bytes/);
-    });
-  });
 });
