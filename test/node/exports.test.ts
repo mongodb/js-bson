f647a5e9d5f4b313896f9fb8dc17f698b336457e
Neal Beeken <neal.beeken@mongodb.com>
2022-11-22 10:00:04 -0500

    test: check EJSON exports as well

--- test/node/exports.test.ts
+++ test/node/exports.test.ts
@@ -67,8 +67,16 @@ const EXPECTED_EXPORTS = [
   'default'
 ];
 
+const EXPECTED_EJSON_EXPORTS = ['parse', 'stringify', 'serialize', 'deserialize'];
+
 describe('bson entrypoint', () => {
   it('should export all and only the expected keys in expected_exports', () => {
     expect(sorted(Object.keys(BSON), byStrings)).to.deep.equal(sorted(EXPECTED_EXPORTS, byStrings));
+  });
+
+  it('should export all and only the expected keys in expected_ejson_exports', () => {
+    expect(sorted(Object.keys(BSON.EJSON), byStrings)).to.deep.equal(
+      sorted(EXPECTED_EJSON_EXPORTS, byStrings)
+    );
   });
 });
