39a5be83b11cb743d723aabce6d07971d8dc8770
Warren James <warren.james@mongodb.com>
2022-12-14 14:27:57 -0500

    test(NODE-4870): test cleanup

--- test/node/bigint.test.ts
+++ test/node/bigint.test.ts
@@ -13,18 +13,18 @@ describe('BSON BigInt serialization Support', function () {
   });
 
   it('Serializes bigints into little-endian byte order', function () {
-    const testDoc = { a: BigInt(0x1234567812345678n) };
+    const testDoc = { a: 0x1234567812345678n };
     const serializedDoc = BSON.serialize(testDoc);
     const expectedResult = byteUtils.bufferFromHexArray([
       '12', // int64 type
       '6100', // 'a' key with key null terminator
-      '7856341278563412' // little endian int64
+      '7856341278563412'
     ]);
     expect(serializedDoc).to.deep.equal(expectedResult);
   });
 
   it('Correctly serializes a BigInt that can be safely represented as a Number', function () {
-    const testDoc = { a: BigInt(0x23) };
+    const testDoc = { a: 0x23n };
     const serializedDoc = BSON.serialize(testDoc);
     const expectedResult = byteUtils.bufferFromHexArray([
       '12', // int64 type
@@ -35,53 +35,53 @@ describe('BSON BigInt serialization Support', function () {
   });
 
   it('Correctly serializes a BigInt in the valid range [-2^63, 2^63 - 1]', function () {
-    const testDoc = { b: BigInt(0xfffffffffffffff1n) };
+    const testDoc = { a: 0xfffffffffffffff1n };
     const serializedDoc = BSON.serialize(testDoc);
     const expectedResult = byteUtils.bufferFromHexArray([
       '12', // int64
-      '6200', // 'b' '\0'
+      '6100', // 'a' key with key null terminator
       'f1ffffffffffffff'
     ]);
     expect(serializedDoc).to.deep.equal(expectedResult);
   });
 
   it('Correctly wraps to negative on a BigInt that is larger than (2^63 -1)', function () {
-    const maxIntPlusOne = { test: 2n ** 63n };
+    const maxIntPlusOne = { a: 2n ** 63n };
     const serializedMaxIntPlusOne = BSON.serialize(maxIntPlusOne);
     const expectedResultForMaxIntPlusOne = byteUtils.bufferFromHexArray([
       '12', // int64
-      Buffer.from('test\x00', 'utf8').toString('hex'),
+      '6100', // 'a' key with key null terminator
       '0000000000000080'
     ]);
     expect(serializedMaxIntPlusOne).to.deep.equal(expectedResultForMaxIntPlusOne);
   });
 
   it('Correctly serializes BigInts at the edges of the valid range [-2^63, 2^63 - 1]', function () {
-    const maxPositiveInt64 = { test: 2n ** 63n - 1n };
+    const maxPositiveInt64 = { a: 2n ** 63n - 1n };
     const serializedMaxPositiveInt64 = BSON.serialize(maxPositiveInt64);
     const expectedSerializationForMaxPositiveInt64 = byteUtils.bufferFromHexArray([
       '12', // int64
-      Buffer.from('test\x00', 'utf8').toString('hex'),
+      '6100', // 'a' key with key null terminator
       'ffffffffffffff7f'
     ]);
     expect(serializedMaxPositiveInt64).to.deep.equal(expectedSerializationForMaxPositiveInt64);
 
-    const minPositiveInt64 = { test: -(2n ** 63n) };
+    const minPositiveInt64 = { a: -(2n ** 63n) };
     const serializedMinPositiveInt64 = BSON.serialize(minPositiveInt64);
     const expectedSerializationForMinPositiveInt64 = byteUtils.bufferFromHexArray([
       '12', // int64
-      Buffer.from('test\x00', 'utf8').toString('hex'),
+      '6100', // 'a' key with key null terminator
       '0000000000000080'
     ]);
     expect(serializedMinPositiveInt64).to.deep.equal(expectedSerializationForMinPositiveInt64);
   });
 
   it('Correctly truncates a BigInt that is larger than a 64-bit int', function () {
-    const testDoc = { test: 2n ** 64n + 1n };
+    const testDoc = { a: 2n ** 64n + 1n };
     const serializedDoc = BSON.serialize(testDoc);
     const expectedSerialization = byteUtils.bufferFromHexArray([
       '12', //int64
-      Buffer.from('test\x00', 'utf8').toString('hex'),
+      '6100', // 'a' key with key null terminator
       '0100000000000000'
     ]);
     expect(serializedDoc).to.deep.equal(expectedSerialization);
