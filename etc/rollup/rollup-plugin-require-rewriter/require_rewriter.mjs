c49da0de01a62bbf5038b65e366a1a195ce5ed3e
Neal Beeken <neal.beeken@mongodb.com>
2022-12-14 14:24:52 -0500

    fixup require to use protocol import

--- etc/rollup/rollup-plugin-require-rewriter/require_rewriter.mjs
+++ etc/rollup/rollup-plugin-require-rewriter/require_rewriter.mjs
@@ -23,12 +23,12 @@ export class RequireRewriter {
     }
 
     const start = code.indexOf('const nodejsRandomBytes');
-    const endString = `return require('crypto').randomBytes;`;
+    const endString = `return require('node:crypto').randomBytes;`;
     const end = code.indexOf(endString) + endString.length;
 
     if (start < 0 || end < 0) {
       throw new Error(
-        `Unexpected! 'const nodejsRandomBytes' or 'return require('crypto').randomBytes;' not found`
+        `Unexpected! 'const nodejsRandomBytes' or 'return require('node:crypto').randomBytes;' not found`
       );
     }
 
