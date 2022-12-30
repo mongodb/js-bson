d152ad5270dec4a8cbc496d7d94a8f6aa839d4b7
Warren James <warren.james@mongodb.com>
2022-12-30 10:43:44 -0500

    test(NODE-4871): Update table tests

--- test/node/bigint.test.ts
+++ test/node/bigint.test.ts
@@ -2,174 +2,138 @@ import { BSON, BSONError } from '../register-bson';
 import { bufferFromHexArray } from './tools/utils';
 import { expect } from 'chai';
 
-describe('BSON BigInt deserialization support', function() {
-  describe('BSON.deserialize()', function() {
-    const testSerializedDoc = bufferFromHexArray(['12', '6100', '2300000000000000']);
+describe('BSON BigInt deserialization support', function () {
+  describe('BSON.deserialize()', function () {
+    const testSerializedDoc = bufferFromHexArray(['12', '6100', '2300000000000000']); // key 'a', value 0x23 as int64
     type DeserialzationOptions = {
-      useBigInt64: boolean | undefined,
-      promoteValues: boolean | undefined,
-      promoteLongs: boolean | undefined,
+      useBigInt64: boolean | undefined;
+      promoteValues: boolean | undefined;
+      promoteLongs: boolean | undefined;
     };
     type TestTableEntry = {
-      options: DeserialzationOptions,
-      isThrowTest: boolean,
-      expectedResult?: BSON.Document,
-      expectedError?: Error
+      options: DeserialzationOptions;
+      shouldThrow: boolean;
+      expectedResult?: BSON.Document;
+      expectedErrorMessage?: string;
     };
     const testTable = [
-      { options: { useBigInt64: undefined, promoteValues: undefined, promoteLongs: undefined }, isThrowTest: false, expectedResult: { a: 0x23 } },
-      { options: { useBigInt64: true, promoteValues: undefined, promoteLongs: undefined }, isThrowTest: false, expectedResult: { a: 0x23n } },
-      { options: { useBigInt64: true, promoteValues: undefined, promoteLongs: undefined }, isThrowTest: false, expectedResult: { a: 0x23n } },
-      { options: { useBigInt64: true, promoteValues: undefined, promoteLongs: true }, isThrowTest: false, expectedResult: { a: 0x23n } },
-      { options: { useBigInt64: false, promoteValues: undefined, promoteLongs: true }, isThrowTest: false, expectedResult: { a: 0x23 } },
-      { options: { useBigInt64: false, promoteValues: undefined, promoteLongs: undefined }, isThrowTest: false, expectedResult: { a: 0x23 } },
-      { options: { useBigInt64: false, promoteValues: undefined, promoteLongs: false }, isThrowTest: false, expectedResult: { a: new BSON.Long(0x23) } },
-      { options: { useBigInt64: undefined, promoteValues: undefined, promoteLongs: false }, isThrowTest: false, expectedResult: { a: new BSON.Long(0x23) } },
-      { options: { useBigInt64: undefined, promoteValues: false, promoteLongs: undefined }, isThrowTest: false, expectedResult: { a: new BSON.Long(0x23) } },
-      { options: { useBigInt64: false, promoteValues: false, promoteLongs: undefined }, isThrowTest: false, expectedResult: { a: new BSON.Long(0x23) } },
-      { options: { useBigInt64: false, promoteValues: false, promoteLongs: false }, isThrowTest: false, expectedResult: { a: new BSON.Long(0x23) } },
-      { options: { useBigInt64: true, promoteValues: true, promoteLongs: true }, isThrowTest: false, expectedResult: { a: 0x23n } },
-      { options: { useBigInt64: false, promoteValues: undefined, promoteLongs: false }, isThrowTest: false, expectedResult: { a: new BSON.Long(0x23) } },
-      { options: { useBigInt64: true, promoteValues: undefined, promoteLongs: false }, isThrowTest: true, expectedError: new BSONError("") },
-      { options: { useBigInt64: true, promoteValues: false, promoteLongs: undefined }, isThrowTest: true, expectedError: new BSONError("") },
+      {
+        options: { useBigInt64: undefined, promoteValues: undefined, promoteLongs: undefined },
+        shouldThrow: false,
+        expectedResult: { a: 0x23 }
+      },
+      {
+        options: { useBigInt64: true, promoteValues: undefined, promoteLongs: undefined },
+        shouldThrow: false,
+        expectedResult: { a: 0x23n }
+      },
+      {
+        options: { useBigInt64: true, promoteValues: false, promoteLongs: undefined },
+        shouldThrow: true,
+        expectedErrorMessage: 'Must either request bigint or Long for int64 deserialization'
+      },
+      {
+        options: { useBigInt64: true, promoteValues: undefined, promoteLongs: true },
+        shouldThrow: false,
+        expectedResult: { a: 0x23n }
+      },
+      {
+        options: { useBigInt64: false, promoteValues: undefined, promoteLongs: true },
+        shouldThrow: false,
+        expectedResult: { a: 0x23 }
+      },
+      {
+        options: { useBigInt64: false, promoteValues: undefined, promoteLongs: undefined },
+        shouldThrow: false,
+        expectedResult: { a: 0x23 }
+      },
+      {
+        options: { useBigInt64: false, promoteValues: undefined, promoteLongs: false },
+        shouldThrow: false,
+        expectedResult: { a: new BSON.Long(0x23) }
+      },
+      {
+        options: { useBigInt64: undefined, promoteValues: undefined, promoteLongs: false },
+        shouldThrow: false,
+        expectedResult: { a: new BSON.Long(0x23) }
+      },
+      {
+        options: { useBigInt64: undefined, promoteValues: false, promoteLongs: undefined },
+        shouldThrow: false,
+        expectedResult: { a: new BSON.Long(0x23) }
+      },
+      {
+        options: { useBigInt64: false, promoteValues: false, promoteLongs: undefined },
+        shouldThrow: false,
+        expectedResult: { a: new BSON.Long(0x23) }
+      },
+      {
+        options: { useBigInt64: false, promoteValues: false, promoteLongs: false },
+        shouldThrow: false,
+        expectedResult: { a: new BSON.Long(0x23) }
+      },
+      {
+        options: { useBigInt64: false, promoteValues: true, promoteLongs: true },
+        shouldThrow: false,
+        expectedResult: { a: 0x23 }
+      },
+      {
+        options: { useBigInt64: true, promoteValues: true, promoteLongs: true },
+        shouldThrow: false,
+        expectedResult: { a: 0x23n }
+      },
+      {
+        options: { useBigInt64: false, promoteValues: undefined, promoteLongs: false },
+        shouldThrow: false,
+        expectedResult: { a: new BSON.Long(0x23) }
+      },
+      {
+        options: { useBigInt64: true, promoteValues: undefined, promoteLongs: false },
+        shouldThrow: true,
+        expectedErrorMessage: 'Must either request bigint or Long for int64 deserialization'
+      }
     ];
 
     function generateTestDescription(entry: TestTableEntry): string {
-      const promoteValues = `promoteValues ${entry.promoteValues == undefined ? 'is default' : `== ${entry.promoteValues}`}`;
-      const promoteLongs = `promoteLongs ${entry.promoteLongs == undefined ? 'is default' : `== ${entry.promoteLongs}`}`;
-      const useBigInt64 = `useBigInt64 ${entry.useBigInt64 == undefined ? 'is default' : `== ${entry.useBigInt64}`}`;
-      const flagString = ` ${useBigInt64}, ${promoteValues}, and ${promoteLongs}`
-      if (entry.isThrowTest) {
+      const options = entry.options;
+      const promoteValues = `promoteValues ${
+        options.promoteValues === undefined ? 'is default' : `is ${options.promoteValues}`
+      }`;
+      const promoteLongs = `promoteLongs ${
+        options.promoteLongs === undefined ? 'is default' : `is ${options.promoteLongs}`
+      }`;
+      const useBigInt64 = `useBigInt64 ${
+        options.useBigInt64 === undefined ? 'is default' : `is ${options.useBigInt64}`
+      }`;
+      const flagString = `${useBigInt64}, ${promoteValues}, and ${promoteLongs}`;
+      if (entry.shouldThrow) {
         return `throws when ${flagString}`;
       } else {
-        return `deserializes int64 to ${typeof entry.expectedResult} when ${flagString}`
+        return `deserializes int64 to ${typeof entry.expectedResult} when ${flagString}`;
       }
     }
 
-    function generateTest(entry: TestTableEntry) {
-      const options = entry.options;
-      if (entry.isThrowTest) {
-        return function() {
-          expect(function() {
+    function generateTest(test: TestTableEntry) {
+      const options = test.options;
+      if (test.shouldThrow) {
+        return function () {
+          expect(function () {
             BSON.deserialize(testSerializedDoc, options);
-          }).to.throw(entry.expectedError?.name)
-        }
+          }).to.throw(BSONError, test.expectedErrorMessage ?? '');
+        };
       } else {
         return () => {
           const deserializedDoc = BSON.deserialize(testSerializedDoc, options);
-          expect(deserializedDoc).to.deep.equal(entry.expectedResult);
-        }
+          expect(deserializedDoc).to.deep.equal(test.expectedResult);
+        };
       }
     }
 
-    for (let entry of testTable) {
-      let test = generateTest(entry);
-      let description = generateTestDescription(entry);
+    for (const entry of testTable) {
+      const test = generateTest(entry);
+      const description = generateTestDescription(entry);
 
       it(description, test);
     }
-
-    // TODO(NODE-4871): Check for duplicated tests
-    // TODO(NODE-4871): Ensure all desired test cases are covered
-    /*
-    it('deserializes int64 to Number when useBigInt64,promoteValues, promoteLongs are default', function() {
-      const deserializedDoc = BSON.deserialize(testSerializedDoc);
-      expect(deserializedDoc).to.deep.equal({ a: 0x23 });
-    });
-
-    it('deserializes int64 to BigInt when useBigInt64 == true, promoteValues, promoteLongs are default', function() {
-      const deserializedDoc = BSON.deserialize(testSerializedDoc, { useBigInt64: true });
-      expect(deserializedDoc).to.deep.equal({ a: 0x23n });
-    });
-
-    it('deserializes int64 to BigInt when useBigInt64 == true and promoteLongs == true', function() {
-      const deserializedDoc = BSON.deserialize(testSerializedDoc, {
-        useBigInt64: true,
-        promoteLongs: true
-      });
-      expect(deserializedDoc).to.deep.equal({ a: 0x23n });
-    });
-
-    it('deserializes int64 to Number when useBigInt64 == false and promoteLongs == true', function() {
-      const deserializedDoc = BSON.deserialize(testSerializedDoc, {
-        useBigInt64: false,
-        promoteLongs: true
-      });
-      expect(deserializedDoc).to.deep.equal({ a: 0x23 });
-    });
-
-    it('deserializes int64 to Number when useBigInt64 == false and promoteLongs is default', function() {
-      const deserializedDoc = BSON.deserialize(testSerializedDoc, { useBigInt64: false });
-      expect(deserializedDoc).to.deep.equal({ a: 0x23 });
-    });
-
-    it('deserializes int64 to BSON.Long when useBigInt64 == false and promoteLongs == false', function() {
-      const deserializedDoc = BSON.deserialize(testSerializedDoc, {
-        useBigInt64: false,
-        promoteLongs: false
-      });
-      expect(deserializedDoc).to.deep.equal({ a: new BSON.Long(0x23) });
-    });
-
-    it('deserializes int64 to BSON.Long when useBigInt64 is default and promoteValues == false', function() {
-      const deserializedDoc = BSON.deserialize(testSerializedDoc, { promoteValues: false });
-      expect(deserializedDoc).to.deep.equal({ a: new BSON.Long(0x23) });
-    });
-
-    it('deserializes int64 to BSON.Long when useBigInt64 == false and promoteValues == false', function() {
-      const deserializedDoc = BSON.deserialize(testSerializedDoc, {
-        useBigInt64: false,
-        promoteValues: false
-      });
-      expect(deserializedDoc).to.deep.equal({ a: new BSON.Long(0x23) });
-    });
-
-    it('deserializes int64 to BSON.Long when useBigInt64 == false, promoteLongs == false, and promoteValues == false', function() {
-      const deserializedDoc = BSON.deserialize(testSerializedDoc, {
-        useBigInt64: false,
-        promoteLongs: false,
-        promoteValues: false
-      });
-      expect(deserializedDoc).to.deep.equal({ a: new BSON.Long(0x23) });
-    });
-
-    it('deserializes int64 to Number when useBigInt64 == false, promoteLongs == true, and promoteValues == true', function() {
-      const deserializedDoc = BSON.deserialize(testSerializedDoc, {
-        useBigInt64: false,
-        promoteLongs: true,
-        promoteValues: true
-      });
-      expect(deserializedDoc).to.deep.equal({ a: 0x23 });
-    });
-
-    it('deserializes int64 to BigInt when useBigInt64 == true, promoteLongs == true, and promoteValues == true', function() {
-      const deserializedDoc = BSON.deserialize(testSerializedDoc, {
-        useBigInt64: true,
-        promoteLongs: true,
-        promoteValues: true
-      });
-      expect(deserializedDoc).to.deep.equal({ a: 0x23n });
-    });
-
-    it('deserializes int64 to BSON.Long when useBigInt64 == false and promoteLongs == false', function() {
-      const deserializedDoc = BSON.deserialize(testSerializedDoc, {
-        useBigInt64: false,
-        promoteLongs: false
-      });
-      expect(deserializedDoc).to.deep.equal({ a: new BSON.Long(0x23) });
-    });
-
-    it('throws error when useBigInt64 == true and promoteLongs == false', function() {
-      expect(function() {
-        BSON.deserialize(testSerializedDoc, { useBigInt64: true, promoteLongs: false });
-      }).to.throw(BSON.BSONError);
-    });
-
-    it('throws error when useBigInt64 == true and promoteValues == false', function() {
-      expect(function() {
-        BSON.deserialize(testSerializedDoc, { useBigInt64: true, promoteValues: false });
-      }).to.throw(BSON.BSONError);
-    });
-    */
   });
 });
