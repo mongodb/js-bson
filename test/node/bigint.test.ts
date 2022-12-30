e02060a2f7639304d586174cb06df045c59e18db
Warren James <warren.james@mongodb.com>
2022-12-30 11:40:46 -0500

    test(NODE-4871): Generate table tests

--- test/node/bigint.test.ts
+++ test/node/bigint.test.ts
@@ -2,9 +2,8 @@ import { BSON, BSONError } from '../register-bson';
 import { bufferFromHexArray } from './tools/utils';
 import { expect } from 'chai';
 
-describe('BSON BigInt deserialization support', function () {
-  describe('BSON.deserialize()', function () {
-    const testSerializedDoc = bufferFromHexArray(['12', '6100', '2300000000000000']); // key 'a', value 0x23 as int64
+describe('BSON BigInt deserialization support', function() {
+  describe('BSON.deserialize()', function() {
     type DeserialzationOptions = {
       useBigInt64: boolean | undefined;
       promoteValues: boolean | undefined;
@@ -13,111 +12,71 @@ describe('BSON BigInt deserialization support', function () {
     type TestTableEntry = {
       options: DeserialzationOptions;
       shouldThrow: boolean;
-      expectedResult?: BSON.Document;
-      expectedErrorMessage?: string;
+      expectedResult: BSON.Document;
+      expectedErrorMessage: string;
     };
-    const testTable = [
-      {
-        options: { useBigInt64: undefined, promoteValues: undefined, promoteLongs: undefined },
-        shouldThrow: false,
-        expectedResult: { a: 0x23 }
-      },
-      {
-        options: { useBigInt64: true, promoteValues: undefined, promoteLongs: undefined },
-        shouldThrow: false,
-        expectedResult: { a: 0x23n }
-      },
-      {
-        options: { useBigInt64: true, promoteValues: false, promoteLongs: undefined },
-        shouldThrow: true,
-        expectedErrorMessage: 'Must either request bigint or Long for int64 deserialization'
-      },
-      {
-        options: { useBigInt64: true, promoteValues: undefined, promoteLongs: true },
-        shouldThrow: false,
-        expectedResult: { a: 0x23n }
-      },
-      {
-        options: { useBigInt64: false, promoteValues: undefined, promoteLongs: true },
-        shouldThrow: false,
-        expectedResult: { a: 0x23 }
-      },
-      {
-        options: { useBigInt64: false, promoteValues: undefined, promoteLongs: undefined },
-        shouldThrow: false,
-        expectedResult: { a: 0x23 }
-      },
-      {
-        options: { useBigInt64: false, promoteValues: undefined, promoteLongs: false },
-        shouldThrow: false,
-        expectedResult: { a: new BSON.Long(0x23) }
-      },
-      {
-        options: { useBigInt64: undefined, promoteValues: undefined, promoteLongs: false },
-        shouldThrow: false,
-        expectedResult: { a: new BSON.Long(0x23) }
-      },
-      {
-        options: { useBigInt64: undefined, promoteValues: false, promoteLongs: undefined },
-        shouldThrow: false,
-        expectedResult: { a: new BSON.Long(0x23) }
-      },
-      {
-        options: { useBigInt64: false, promoteValues: false, promoteLongs: undefined },
-        shouldThrow: false,
-        expectedResult: { a: new BSON.Long(0x23) }
-      },
-      {
-        options: { useBigInt64: false, promoteValues: false, promoteLongs: false },
-        shouldThrow: false,
-        expectedResult: { a: new BSON.Long(0x23) }
-      },
-      {
-        options: { useBigInt64: false, promoteValues: true, promoteLongs: true },
-        shouldThrow: false,
-        expectedResult: { a: 0x23 }
-      },
-      {
-        options: { useBigInt64: true, promoteValues: true, promoteLongs: true },
-        shouldThrow: false,
-        expectedResult: { a: 0x23n }
-      },
-      {
-        options: { useBigInt64: false, promoteValues: undefined, promoteLongs: false },
-        shouldThrow: false,
-        expectedResult: { a: new BSON.Long(0x23) }
-      },
-      {
-        options: { useBigInt64: true, promoteValues: undefined, promoteLongs: false },
-        shouldThrow: true,
-        expectedErrorMessage: 'Must either request bigint or Long for int64 deserialization'
-      }
-    ];
+    const testSerializedDoc = bufferFromHexArray(['12', '6100', '2300000000000000']); // key 'a', value 0x23 as int64
+    const useBigInt64Values = [true, false, undefined];
+    const promoteLongsValues = [true, false, undefined];
+    const promoteValuesValues = [true, false, undefined];
+
+    const tests = useBigInt64Values.flatMap(useBigInt64 => {
+      return promoteLongsValues.flatMap(promoteLongs => {
+        return promoteValuesValues.flatMap(promoteValues => {
+          const useBigInt64Set = useBigInt64 ?? false;
+          const promoteLongsSet = promoteLongs ?? true;
+          const promoteValuesSet = promoteValues ?? true;
+          const shouldThrow = useBigInt64Set && (!promoteValuesSet || !promoteLongsSet);
+          let expectedResult: BSON.Document;
+          if (useBigInt64Set) {
+            expectedResult = { a: 0x23n };
+          } else if (promoteLongsSet && promoteValuesSet) {
+            expectedResult = { a: 0x23 };
+          } else {
+            expectedResult = { a: new BSON.Long(0x23) };
+          }
+          const expectedErrorMessage = shouldThrow
+            ? 'Must either request bigint or Long for int64 deserialization'
+            : '';
+          return [
+            {
+              options: { useBigInt64, promoteValues, promoteLongs },
+              shouldThrow,
+              expectedResult,
+              expectedErrorMessage
+            }
+          ];
+        });
+      });
+    });
+
+    it('meta test: should test table correctly', () => {
+      expect(tests).to.have.lengthOf(27);
+      expect(tests.filter(t => t.shouldThrow)).to.have.lengthOf(5);
+      expect(tests.filter(t => !t.shouldThrow)).to.have.lengthOf(22);
+    });
 
     function generateTestDescription(entry: TestTableEntry): string {
       const options = entry.options;
-      const promoteValues = `promoteValues ${
-        options.promoteValues === undefined ? 'is default' : `is ${options.promoteValues}`
-      }`;
-      const promoteLongs = `promoteLongs ${
-        options.promoteLongs === undefined ? 'is default' : `is ${options.promoteLongs}`
-      }`;
-      const useBigInt64 = `useBigInt64 ${
-        options.useBigInt64 === undefined ? 'is default' : `is ${options.useBigInt64}`
-      }`;
+      const promoteValues = `promoteValues ${options.promoteValues === undefined ? 'is default' : `is ${options.promoteValues}`
+        }`;
+      const promoteLongs = `promoteLongs ${options.promoteLongs === undefined ? 'is default' : `is ${options.promoteLongs}`
+        }`;
+      const useBigInt64 = `useBigInt64 ${options.useBigInt64 === undefined ? 'is default' : `is ${options.useBigInt64}`
+        }`;
       const flagString = `${useBigInt64}, ${promoteValues}, and ${promoteLongs}`;
       if (entry.shouldThrow) {
         return `throws when ${flagString}`;
       } else {
-        return `deserializes int64 to ${typeof entry.expectedResult} when ${flagString}`;
+        return `deserializes int64 to ${entry.expectedResult.a.constructor.name} when ${flagString}`;
       }
     }
 
     function generateTest(test: TestTableEntry) {
       const options = test.options;
       if (test.shouldThrow) {
-        return function () {
-          expect(function () {
+        return () => {
+          expect(() => {
             BSON.deserialize(testSerializedDoc, options);
           }).to.throw(BSONError, test.expectedErrorMessage ?? '');
         };
@@ -129,7 +88,7 @@ describe('BSON BigInt deserialization support', function () {
       }
     }
 
-    for (const entry of testTable) {
+    for (const entry of tests) {
       const test = generateTest(entry);
       const description = generateTestDescription(entry);
 
