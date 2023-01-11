260b6672d876ee5ccafb86e7e490e99d23aff5f2
Warren James <warren.james@mongodb.com>
2023-01-11 11:45:23 -0500

    test(NODE-4874): Add test to check that legacy flag doesn't affect output of Long.fromExtendedJSON

--- test/node/bigint.test.ts
+++ test/node/bigint.test.ts
@@ -274,6 +274,7 @@ describe('BSON BigInt support', function () {
       expectedResult: BSON.Document;
     };
 
+    // NOTE: legacy is not changed here as it does not affect the output of parsing a Long/Number
     const useBigInt64Values = [true, false, undefined];
     const relaxedValues = [true, false, undefined];
     const sampleCanonicalString = '{"a":{"$numberLong":"23"}}';
--- test/node/long.test.ts
+++ test/node/long.test.ts
@@ -1,7 +1,8 @@
+import { expect } from 'chai';
 import { Long } from '../register-bson';
 
-describe('Long', function () {
-  it('accepts strings in the constructor', function () {
+describe('Long', function() {
+  it('accepts strings in the constructor', function() {
     expect(new Long('0').toString()).to.equal('0');
     expect(new Long('00').toString()).to.equal('0');
     expect(new Long('-1').toString()).to.equal('-1');
@@ -12,7 +13,7 @@ describe('Long', function () {
     expect(new Long('13835058055282163712', true).toString()).to.equal('13835058055282163712');
   });
 
-  it('accepts BigInts in Long constructor', function () {
+  it('accepts BigInts in Long constructor', function() {
     expect(new Long(0n).toString()).to.equal('0');
     expect(new Long(-1n).toString()).to.equal('-1');
     expect(new Long(-1n, true).toString()).to.equal('18446744073709551615');
@@ -20,5 +21,15 @@ describe('Long', function () {
     expect(new Long(123456789123456789n, true).toString()).to.equal('123456789123456789');
     expect(new Long(13835058055282163712n).toString()).to.equal('-4611686018427387904');
     expect(new Long(13835058055282163712n, true).toString()).to.equal('13835058055282163712');
+  });
+
+  describe('static fromExtendedJSON()', function() {
+    it('is not affected by the legacy flag', function() {
+      const ejsonDoc = { $numberLong: "123456789123456789" };
+      const longLegacy = Long.fromExtendedJSON(ejsonDoc, { legacy: true });
+      const longNonLegacy = Long.fromExtendedJSON(ejsonDoc, { legacy: false });
+
+      expect(longLegacy).to.deep.equal(longNonLegacy);
+    });
   });
 });
