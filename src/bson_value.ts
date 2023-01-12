38874290d35c53c4d92ee7f70a5bc2da5f828dec
Neal Beeken <neal.beeken@mongodb.com>
2023-01-11 20:59:03 -0500

    fix: add access annotations

--- src/bson_value.ts
+++ src/bson_value.ts
@@ -2,13 +2,17 @@ import { BSON_MAJOR_VERSION } from './constants';
 
 /** @public */
 export abstract class BSONValue {
-  abstract get _bsontype(): string;
+  /** @public */
+  public abstract get _bsontype(): string;
 
   /** @internal */
   get [Symbol.for('@@mdb.bson.version')](): BSON_MAJOR_VERSION {
     return BSON_MAJOR_VERSION;
   }
 
-  abstract inspect(): string;
+  /** @public */
+  public abstract inspect(): string;
+
+  /** @internal */
   abstract toExtendedJSON(): unknown;
 }
