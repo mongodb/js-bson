600df2b3d2c79d8fe1d614aa91e3dead4dba766b
Warren James <warren.james@mongodb.com>
2023-02-06 16:44:44 -0500

    chore(NODE-4976): Move responsibility to report on uses of BigInt static methods and constructor to eslint builtin rule

--- etc/eslint/no-bigint-usage/index.js
+++ etc/eslint/no-bigint-usage/index.js
@@ -13,30 +13,8 @@ function noBigIntLiterals(context) {
   };
 }
 
-function noCallsToBigIntMethods(context) {
-  return {
-    CallExpression(node) {
-      if (node.callee.type === 'Identifier' && node.callee.name === 'BigInt') {
-        context.report({
-          node,
-          message: 'Calls to BigInt not allowed'
-        });
-      } else if (node.callee.type === 'MemberExpression' &&
-        node.callee.object.type === 'Identifier'
-        && node.callee.object.name === 'BigInt'
-      ) {
-        context.report({
-          node,
-          message: 'Calls to BigInt static methods not allowed'
-        });
-      }
-    }
-  };
-}
-
 module.exports = {
   rules: {
-    'no-bigint-literals': { create: noBigIntLiterals },
-    'no-calls-to-BigInt-methods': { create: noCallsToBigIntMethods }
+    'no-bigint-literals': { create: noBigIntLiterals }
   }
 }
