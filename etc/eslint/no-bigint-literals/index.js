08e1a8c97753d01704ab62df29b6f46e666e7dd2
Warren James <warren.james@mongodb.com>
2023-02-06 09:46:28 -0500

    chore(NODE-4976): Update eslint plugin

--- etc/eslint/no-bigint-literals/index.js
+++ etc/eslint/no-bigint-literals/index.js
@@ -1,20 +1,42 @@
-module.exports = {
-  rules: {
-    "no-bigint-literals": {
-      create: function(context) {
-        return {
-          Literal(node) {
-            if (node.bigint && /n$/.test(node.raw)) {
-              context.report(
-                {
-                  node,
-                  message: "BigInt literals not allowed"
-                }
-              )
-            }
+function noBigIntLiterals(context) {
+  return {
+    Literal(node) {
+      if (node.bigint && /n$/.test(node.raw)) {
+        context.report(
+          {
+            node,
+            message: 'BigInt literals not allowed'
           }
-        }
+        )
       }
     }
+  };
+}
+
+function noCallsToBigIntMethods(context) {
+  return {
+    CallExpression(node) {
+      if (node.callee.type === 'Identifier' && node.callee.name === 'BigInt') {
+        context.report({
+          node,
+          message: 'Calls to BigInt not allowed'
+        });
+      } else if (node.callee.type === 'MemberExpression' &&
+        node.callee.object.type === 'Identifier'
+        && node.callee.object.name === 'BigInt'
+      ) {
+        context.report({
+          node,
+          message: 'Calls to BigInt static methods not allowed'
+        });
+      }
+    }
+  };
+}
+
+module.exports = {
+  rules: {
+    'no-bigint-literals': { create: noBigIntLiterals },
+    'no-calls-to-BigInt-methods': { create: noCallsToBigIntMethods }
   }
 }
--- etc/eslint/no-bigint-literals/test/no_bigint_literals.test.js
+++ etc/eslint/no-bigint-literals/test/no_bigint_literals.test.js
@@ -3,12 +3,14 @@ const { RuleTester } = require('eslint');
 
 const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2020 } });
 
-ruleTester.run("no-bigint-literals", rules['no-bigint-literals'], {
+ruleTester.run('no-bigint-literals', rules['no-bigint-literals'], {
   valid: [{
-    code: "let bigint = BigInt(10);"
+    name: 'Accepts calls to BigInt constructor',
+    code: 'let bigint = BigInt(10);'
   }],
   invalid: [{
-    code: "let bigint = 10n;",
+    name: 'Rejects use of bigint literals',
+    code: 'let bigint = 10n;',
     errors: 1
   }],
 });
