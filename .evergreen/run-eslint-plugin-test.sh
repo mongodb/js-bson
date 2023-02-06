f49bbd7f6b2a2996aeaabcac6819820c86550f71
Warren James <warren.james@mongodb.com>
2023-02-06 13:20:58 -0500

    chore(NODE-4976): ci WIP

---
+++ .evergreen/run-eslint-plugin-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$NODE_VERSION" ]; then
+  echo "NODE_VERSION environment variable must be specified"
+  exit 1
+fi
+
+NODE_ARTIFACTS_PATH="${PROJECT_DIRECTORY}/node-artifacts"
+
+export PATH="/opt/mongodbtoolchain/v2/bin:$PATH"
+export NVM_DIR="${NODE_ARTIFACTS_PATH}/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+cd src/etc/eslint/no-bigint-usage
+npm install
+npm run test
