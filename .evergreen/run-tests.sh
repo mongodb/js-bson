a6f3684df55125c008ab090d3e00da5b7acad97c
Neal Beeken <neal.beeken@mongodb.com>
2023-03-03 16:30:45 -0500

    fix init-nvm usage

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -1,15 +1,6 @@
-#!/bin/bash
-if [ -z "$NODE_VERSION" ]; then
-  echo "NODE_VERSION environment variable must be specified"
-  exit 1
-fi
+#!/usr/bin/env bash
 
-NODE_ARTIFACTS_PATH="${PROJECT_DIRECTORY}/node-artifacts"
-
-export PATH="/opt/mongodbtoolchain/v2/bin:$PATH"
-export NVM_DIR="${NODE_ARTIFACTS_PATH}/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
+source "${PROJECT_DIRECTORY}/.evergreen/init-nvm.sh"
 
 case $1 in
   "node")
