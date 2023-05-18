36b4c8fa8ccefd942fe1f81f9d315c2b135ffc86
Neal Beeken <neal.beeken@mongodb.com>
2023-05-18 13:23:37 -0400

    ci(NODE-5271): add node 20 (#577)

--- .evergreen/setup-environment.sh
+++ .evergreen/setup-environment.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
-if [ -z "$NODE_LTS_NAME" ]; then
-  echo "NODE_LTS_NAME environment variable must be specified"
+if [ -z "$NODE_LTS_VERSION" ]; then
+  echo "NODE_LTS_VERSION environment variable must be specified"
   exit 1
 fi
 
@@ -16,12 +16,12 @@ export PROJECT_DIRECTORY="$(pwd)"
 cat <<EOT > expansion.yml
 CURRENT_VERSION: "$CURRENT_VERSION"
 PROJECT_DIRECTORY: "$PROJECT_DIRECTORY"
-NODE_LTS_NAME: "$NODE_LTS_NAME"
+NODE_LTS_VERSION: "$NODE_LTS_VERSION"
 PREPARE_SHELL: |
     set -o errexit
     set -o xtrace
     export PROJECT_DIRECTORY="$PROJECT_DIRECTORY"
-    export NODE_LTS_NAME="$NODE_LTS_NAME"
+    export NODE_LTS_VERSION="$NODE_LTS_VERSION"
 EOT
 # See what we've done
 cat expansion.yml
