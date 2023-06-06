2c5926615f49dccec3c609877235b0c72ed75995
Bailey Pearson <bailey.pearson@mongodb.com>
2023-06-06 10:55:10 -0600

    rename init-nvm.sh

--- .evergreen/run-checks.sh
+++ .evergreen/run-checks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -o errexit  # Exit the script with error if any of the commands fail
 
-source "${PROJECT_DIRECTORY}/.evergreen/init-nvm.sh"
+source "${PROJECT_DIRECTORY}/.evergreen/init-node-and-npm-env.sh"
 
 npm run check:lint
