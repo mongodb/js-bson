7c6c2fb93704bf3be82621c805450ecd939c158f
Warren James <warren.james@mongodb.com>
2023-07-13 14:48:29 -0400

    ci(NODE-5403): suite name fix

--- test/bench/src/suites/string_deserialization.ts
+++ test/bench/src/suites/string_deserialization.ts
@@ -3,14 +3,14 @@ import * as BSON from '../../../../';
 
 export function getStringDeserializationSuite(): Suite {
   const DOC: Uint8Array = BSON.serialize({
-    nextBatch: Array.from({ length: 100 }, () => {
+    nextBatch: Array.from({ length: 1000 }, () => {
       return {
         _id: new BSON.ObjectId(),
         arrayField: Array.from({ length: 20 }, (_, i) => `5e99f3f5d3ab06936d36000${i}`)
       };
     })
   });
-  const suite = new Suite('string seserialization');
+  const suite = new Suite('string deserialization');
   for (const utf8Validation of [true, false]) {
     suite.task({
       name: `stringDeserializationUTF8${utf8Validation ? 'On' : 'Off'}-${
