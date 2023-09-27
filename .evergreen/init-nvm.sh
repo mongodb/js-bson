5b7414bf9028c6613f9d72a4be8aa022edb76052
Neal Beeken <neal.beeken@mongodb.com>
2023-09-27 14:26:52 -0400

    chore(NODE-5547): fix BSON 4.x CI

--- .evergreen/init-nvm.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-export PATH="/opt/mongodbtoolchain/v2/bin:$PATH"
-NODE_ARTIFACTS_PATH="${PROJECT_DIRECTORY}/node-artifacts"
-export NVM_DIR="${NODE_ARTIFACTS_PATH}/nvm"
-
-[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
-
-export NODE_OPTIONS="--trace-deprecation --trace-warnings"
--- .evergreen/install-dependencies.sh
+++ .evergreen/install-dependencies.sh
@@ -1,59 +1,108 @@
-#!/bin/bash
-if [ -z "$NODE_VERSION" ]; then
-  echo "NODE_VERSION environment variable must be specified"
+#!/usr/bin/env bash
+set -o errexit  # Exit the script with error if any of the commands fail
+
+NODE_LTS_VERSION=${NODE_LTS_VERSION:-16}
+# npm version can be defined in the environment for cases where we need to install
+# a version lower than latest to support EOL Node versions.
+NPM_VERSION=${NPM_VERSION:-latest}
+
+source "${PROJECT_DIRECTORY}/.evergreen/init-node-and-npm-env.sh"
+
+if [[ -z "${npm_global_prefix}" ]]; then echo "npm_global_prefix is unset" && exit 1; fi
+if [[ -z "${NODE_ARTIFACTS_PATH}" ]]; then echo "NODE_ARTIFACTS_PATH is unset" && exit 1; fi
+
+CURL_FLAGS=(
+  --fail          # Exit code 1 if request fails
+  --compressed    # Request a compressed response should keep fetching fast
+  --location      # Follow a redirect
+  --retry 8       # Retry HTTP 408, 429, 500, 502, 503 or 504, 8 times
+  --silent        # Do not print a progress bar
+  --show-error    # Despite the silent flag still print out errors
+  --max-time 900  # 900 seconds is 15 minutes, evergreen times out at 20
+  # --continue-at - # If a download is interrupted it can figure out where to resume
+)
+
+mkdir -p "$NODE_ARTIFACTS_PATH/npm_global"
+
+# Comparisons are all case insensitive
+shopt -s nocasematch
+
+# index.tab is a sorted tab separated values file with the following headers
+# 0       1    2     3   4  5  6    7       8       9   10
+# version date files npm v8 uv zlib openssl modules lts security
+curl "${CURL_FLAGS[@]}" "https://nodejs.org/dist/index.tab" --output node_index.tab
+
+while IFS=$'\t' read -r -a row; do
+  node_index_version="${row[0]}"
+  node_index_major_version=$(echo $node_index_version | sed -E 's/^v([0-9]+).*$/\1/')
+  node_index_date="${row[1]}"
+  node_index_lts="${row[9]}"
+  [[ "$node_index_version" = "version" ]] && continue # skip tsv header
+  [[ "$NODE_LTS_VERSION" = "latest" ]] && break # first line is latest
+  [[ "$NODE_LTS_VERSION" = "$node_index_version" ]] && break # match full version if specified
+  [[ "$NODE_LTS_VERSION" = "$node_index_major_version" ]] && break # case insensitive compare
+done < node_index.tab
+
+if [[ "$OS" = "Windows_NT" ]]; then
+  operating_system="win"
+elif [[ $(uname) = "darwin" ]]; then
+  operating_system="darwin"
+elif [[ $(uname) = "linux" ]]; then
+  operating_system="linux"
+else
+  echo "Unable to determine operating system: $operating_system"
   exit 1
 fi
 
-set -o errexit  # Exit the script with error if any of the commands fail
+architecture=$(uname -m)
+if [[ $architecture = "x86_64" ]]; then
+  architecture="x64"
+elif [[ $architecture = "arm64" ]]; then
+  architecture="arm64"
+elif [[ $architecture = "aarch64" ]]; then
+  architecture="arm64"
+elif [[ $architecture == s390* ]]; then
+  architecture="s390x"
+elif [[ $architecture == ppc* ]]; then
+  architecture="ppc64le"
+else
+  echo "Unable to determine operating system: $architecture"
+  exit 1
+fi
 
-NODE_ARTIFACTS_PATH="${PROJECT_DIRECTORY}/node-artifacts"
-NPM_CACHE_DIR="${NODE_ARTIFACTS_PATH}/npm"
-NPM_TMP_DIR="${NODE_ARTIFACTS_PATH}/tmp"
-BIN_DIR="$(pwd)/bin"
-NVM_URL="https://raw.githubusercontent.com/creationix/nvm/v0.38.0/install.sh"
+file_extension="tar.gz"
+if [[ "$OS" = "Windows_NT" ]]; then file_extension="zip"; fi
 
-# this needs to be explicitly exported for the nvm install below
-export NVM_DIR="${NODE_ARTIFACTS_PATH}/nvm"
+node_directory="node-${node_index_version}-${operating_system}-${architecture}"
+node_archive="${node_directory}.${file_extension}"
+node_archive_path="$NODE_ARTIFACTS_PATH/${node_archive}"
+node_download_url="https://nodejs.org/dist/${node_index_version}/${node_archive}"
 
-# create node artifacts path if needed
-mkdir -p "${NODE_ARTIFACTS_PATH}"
-mkdir -p "${NPM_CACHE_DIR}"
-mkdir -p "${NPM_TMP_DIR}"
-mkdir -p "${BIN_DIR}"
-mkdir -p "${NVM_DIR}"
-
-# Add mongodb toolchain to path
-export PATH="${BIN_DIR}:${PATH}"
-
-# install Node.js
-echo "Installing Node ${NODE_LTS_NAME}"
-
-set +o xtrace
-
-echo "  Downloading nvm"
-curl -o- $NVM_URL | bash
-[ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"
-
-echo "Running: nvm install --lts --latest-npm"
-nvm install --lts --latest-npm
-echo "Running: nvm install ${NODE_VERSION}"
-nvm install "${NODE_VERSION}"
-echo "Running: nvm use --lts"
-nvm use --lts
+echo "Node.js ${node_index_version} for ${operating_system}-${architecture} released on ${node_index_date}"
 
 set -o xtrace
 
+curl "${CURL_FLAGS[@]}" "${node_download_url}" --output "$node_archive_path"
 
+if [[ "$file_extension" = "zip" ]]; then
+  unzip -q "$node_archive_path" -d "${NODE_ARTIFACTS_PATH}"
+  mkdir -p "${NODE_ARTIFACTS_PATH}/nodejs"
+  # Windows "bins" are at the top level
+  mv "${NODE_ARTIFACTS_PATH}/${node_directory}" "${NODE_ARTIFACTS_PATH}/nodejs/bin"
+  # Need to add executable flag ourselves
+  chmod +x "${NODE_ARTIFACTS_PATH}/nodejs/bin/node.exe"
+  chmod +x "${NODE_ARTIFACTS_PATH}/nodejs/bin/npm"
+else
+  tar -xf "$node_archive_path" -C "${NODE_ARTIFACTS_PATH}"
+  mv "${NODE_ARTIFACTS_PATH}/${node_directory}" "${NODE_ARTIFACTS_PATH}/nodejs"
+fi
 
-# setup npm cache in a local directory
-cat <<EOT > .npmrc
-devdir=${NPM_CACHE_DIR}/.node-gyp
-init-module=${NPM_CACHE_DIR}/.npm-init.js
-cache=${NPM_CACHE_DIR}
-tmp=${NPM_TMP_DIR}
-registry=https://registry.npmjs.org
-EOT
+if [[ $operating_system != "win" && $NODE_LTS_VERSION != "6" ]]; then
+  npm install --global npm@$NPM_VERSION
+  hash -r
+fi
 
-# install node dependencies
-npm install # npm prepare runs after install and will compile the library
-nvm use "${NODE_VERSION}" # Switch to the node version we want to test against
+echo "npm location: $(which npm)"
+echo "npm version: $(npm -v)"
+
+npm install "${NPM_OPTIONS}";
