13d83f597653e17787e2e4b8adfd82fb6a826144
Neal Beeken <neal.beeken@mongodb.com>
2023-09-28 18:32:06 -0400

    chore(NODE-5547): fix BSON 4.x CI (#634)

--- .evergreen/run-typescript.sh
+++ .evergreen/run-typescript.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
+
 set -o errexit # Exit the script with error if any of the commands fail
-
-# source "${PROJECT_DIRECTORY}/.evergreen/init-nvm.sh"
-
 set -o xtrace
+
+source "${PROJECT_DIRECTORY}/.evergreen/init-node-and-npm-env.sh"
 
 function get_current_ts_version {
     node -e "console.log(require('./package-lock.json').dependencies.typescript.version)"
