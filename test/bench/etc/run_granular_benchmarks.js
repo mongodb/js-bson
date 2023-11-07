a0f846f848686dd220993a5f487620e4b76c7cc8
Warren James <warren.james@mongodb.com>
2023-11-07 15:23:30 -0500

    update run_granular_benchmarks.js

--- test/bench/etc/run_granular_benchmarks.js
+++ test/bench/etc/run_granular_benchmarks.js
@@ -3,11 +3,12 @@
 const cp = require('child_process');
 const fs = require('fs/promises');
 
-const rx = /(.*)\.bench\.js$/;
+const BENCHMARK_REGEX = /(.*)\.bench\.js$/;
 (async () => {
+  // Run all benchmark files
   const lib = await fs.readdir('../lib/granular');
   for await (const dirent of lib) {
-    if (rx.test(dirent)) {
+    if (BENCHMARK_REGEX.test(dirent)) {
       const child = cp.fork(`../lib/granular/${dirent}`);
       if (child.stdout) child.stdout.pipe(process.stdout);
       if (child.stderr) child.stdout.pipe(process.stderr);
@@ -20,4 +21,32 @@ const rx = /(.*)\.bench\.js$/;
       );
     }
   }
+  const resultPaths = [];
+
+  for await (const dirent of await fs.opendir('./')) {
+    console.log(dirent.name);
+    if (/Results.json$/.test(dirent.name)) {
+      resultPaths.push(`./${dirent.name}`);
+    }
+  }
+
+  // Merge results into one file
+  const collectedResults = await fs.open('resultsCollected.json', 'w+');
+  await collectedResults.write('[\n');
+  for (let i = 0; i < resultPaths.length; i++) {
+    const resultPath = resultPaths[i];
+    const results = require(resultPath);
+    if (Array.isArray(results)) {
+      for (let j = 0; j < results.length; j++) {
+        collectedResults.write(
+          `  ${JSON.stringify(results[j], undefined, 2)}${
+            j === results.length - 1 && i === resultPaths.length - 1 ? '' : ',\n'
+          }`
+        );
+      }
+    }
+  }
+
+  await collectedResults.write(']\n');
+  await collectedResults.close();
 })();
