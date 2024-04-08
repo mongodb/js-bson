eda575a1d0a20c08b215d3d14d8dd6a7039337a7
Warren James <warren.james@mongodb.com>
2024-04-08 00:40:45 -0400

    restore file

--- test/node/string_test.js
+++ test/node/string_test.js
@@ -4,7 +4,7 @@ const BSON = require('../register-bson');
 
 describe('string tests', function () {
   it('can serialize and deserialize 0xFFFD', function () {
-    const unicodeString = String.fromCharCode(0x41, 0x42, 0xfffd, 0x43, 0x44); // "AB\xfffdCD"
+    const unicodeString = String.fromCharCode(0x41, 0x42, 0xfffd, 0x43, 0x44); // "AB�CD"
 
     const serialized = BSON.serialize({ value: unicodeString });
     BSON.deserialize(serialized);
