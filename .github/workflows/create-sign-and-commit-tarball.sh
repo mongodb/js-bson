a5e7e3dcbed1c1783800ec153f9094c1b0cf4864
Aditi Khare <aditi.khare@mongodb.com>
2024-05-08 13:58:16 -0400

    uncomment

--- .github/workflows/create-sign-and-commit-tarball.sh
+++ .github/workflows/create-sign-and-commit-tarball.sh
@@ -11,10 +11,8 @@ echo "Create release tarball"
 npm pack
 echo package version: $PACKAGE_VERSION
 echo gpg key id: $GPG_KEY_ID
-# mv "bson-${PACKAGE_VERSION}.tgz" "bson-${PACKAGE_VERSION}.tgz.${GPG_KEY_ID}"
-
-# git add .
+mv "bson-${PACKAGE_VERSION}.tgz" "bson-${PACKAGE_VERSION}.tgz.${GPG_KEY_ID}"
 
 # Create signed "Package x.y.z" commit
 echo "Create package commit"
-# git commit -m "Package ${PACKAGE_VERSION}" -s --gpg-sign=${GPG_KEY_ID} 
+git commit -m "Package ${PACKAGE_VERSION}" -s --gpg-sign=${GPG_KEY_ID} 
