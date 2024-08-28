426c781b3f944bb100a3e70268f3a17210a1178f
Neal Beeken <neal.beeken@mongodb.com>
2024-08-28 11:40:51 -0400

    Revert "chore(NODE-XXXX): make benchmarks run current code changes"
    This reverts commit 2ab0aec2ff7c5b4dc7acdac649617ddef2504697.

--- .evergreen/run-granular-benchmarks.sh
+++ .evergreen/run-granular-benchmarks.sh
@@ -2,13 +2,6 @@
 
 source "${PROJECT_DIRECTORY}/.evergreen/init-node-and-npm-env.sh"
 set -o xtrace
-
-export LIBRARY=$PWD
-
-if ! npm run build; then exit 1; fi
-
-npm run check:spec-bench
-
 WARMUP=$WARMUP
 ITERATIONS=$ITERATIONS
 
