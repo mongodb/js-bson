2ab0aec2ff7c5b4dc7acdac649617ddef2504697
Neal Beeken <neal.beeken@mongodb.com>
2024-08-28 11:37:48 -0400

    chore(NODE-XXXX): make benchmarks run current code changes

--- .evergreen/run-granular-benchmarks.sh
+++ .evergreen/run-granular-benchmarks.sh
@@ -2,6 +2,13 @@
 
 source "${PROJECT_DIRECTORY}/.evergreen/init-node-and-npm-env.sh"
 set -o xtrace
+
+export LIBRARY=$PWD
+
+if ! npm run build; then exit 1; fi
+
+npm run check:spec-bench
+
 WARMUP=$WARMUP
 ITERATIONS=$ITERATIONS
 
--- .evergreen/run-spec-benchmarks.sh
+++ .evergreen/run-spec-benchmarks.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+
 source "${PROJECT_DIRECTORY}/.evergreen/init-node-and-npm-env.sh"
+
+set -o xtrace
+
+export LIBRARY=$PWD
+
+if ! npm run build; then exit 1; fi
 
 npm run check:spec-bench
