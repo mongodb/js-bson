aa7402d0ad8847253aac2db01ef1fda7d9dca028
Bailey Pearson <bailey.pearson@mongodb.com>
2024-08-28 09:40:37 -0600

    run benchmarks from current branch, not main

--- .evergreen/run-granular-benchmarks.sh
+++ .evergreen/run-granular-benchmarks.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ -z "${PROJECT_DIRECTORY}" ]]; then echo "PROJECT_DIRECTORY is unset" && exit 1; fi
 source "${PROJECT_DIRECTORY}/.evergreen/init-node-and-npm-env.sh"
+
 set -o xtrace
 WARMUP=$WARMUP
 ITERATIONS=$ITERATIONS
 
-WARMUP=$WARMUP ITERATIONS=$ITERATIONS npm run check:granular-bench
+WARMUP=$WARMUP ITERATIONS=$ITERATIONS LIBRARY=$PROJECT_DIRECTORY npm run check:granular-bench
