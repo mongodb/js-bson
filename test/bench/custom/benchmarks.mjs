a7ea8d0ec6267f5dcc9c22fa3d8fd6311ad7be90
Neal Beeken <neal.beeken@mongodb.com>
2024-09-16 15:58:32 -0400

    chore: add better description for short circuiting

--- test/bench/custom/benchmarks.mjs
+++ test/bench/custom/benchmarks.mjs
@@ -5,10 +5,12 @@ const ObjectId_isValid = [
   function objectid_isvalid_strlen() {
     BSON.ObjectId.isValid('a');
   },
-  function objectid_isvalid_bestcase_false() {
+  /** wrong character at the start */
+  function objectid_isvalid_most_short_circuit_false() {
     BSON.ObjectId.isValid('g6e84ebdc96f4c0772f0cbbf');
   },
-  function objectid_isvalid_worstcase_false() {
+  /** wrong character at the end */
+  function objectid_isvalid_least_short_circuit_false() {
     BSON.ObjectId.isValid('66e84ebdc96f4c0772f0cbbg');
   },
   function objectid_isvalid_true() {
