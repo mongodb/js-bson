c08fa565466cd27df6cd0c4eb0b0dae451a40886
Neal Beeken <neal.beeken@mongodb.com>
2024-10-15 19:22:38 -0400

    chore(NODE-6401): migrate node download script to drivers-tools (#718)

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source "${PROJECT_DIRECTORY}/.evergreen/init-node-and-npm-env.sh"
+source $DRIVERS_TOOLS/.evergreen/init-node-and-npm-env.sh
 
 case "${TEST_TARGET}" in
   "node")
