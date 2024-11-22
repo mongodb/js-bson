ced10d73bd2ff2bd0e4a78d0ff1b1ddec32bf0d8
Neal Beeken <neal.beeken@mongodb.com>
2024-11-22 12:41:30 -0500

    chore: remove WeakMap cache of types and use toStringTag getter

--- src/parser/utils.ts
+++ src/parser/utils.ts
@@ -1,27 +1,51 @@
 export function isAnyArrayBuffer(value: unknown): value is ArrayBuffer {
-  return ['[object ArrayBuffer]', '[object SharedArrayBuffer]'].includes(
-    Object.prototype.toString.call(value)
+  return (
+    typeof value === 'object' &&
+    value != null &&
+    Symbol.toStringTag in value &&
+    (value[Symbol.toStringTag] === 'ArrayBuffer' ||
+      value[Symbol.toStringTag] === 'SharedArrayBuffer')
   );
 }
 
 export function isUint8Array(value: unknown): value is Uint8Array {
-  return Object.prototype.toString.call(value) === '[object Uint8Array]';
+  return (
+    typeof value === 'object' &&
+    value != null &&
+    Symbol.toStringTag in value &&
+    value[Symbol.toStringTag] === 'Uint8Array'
+  );
 }
 
 export function isBigInt64Array(value: unknown): value is BigInt64Array {
-  return Object.prototype.toString.call(value) === '[object BigInt64Array]';
+  return (
+    typeof value === 'object' &&
+    value != null &&
+    Symbol.toStringTag in value &&
+    value[Symbol.toStringTag] === 'BigInt64Array'
+  );
 }
 
 export function isBigUInt64Array(value: unknown): value is BigUint64Array {
-  return Object.prototype.toString.call(value) === '[object BigUint64Array]';
+  return (
+    typeof value === 'object' &&
+    value != null &&
+    Symbol.toStringTag in value &&
+    value[Symbol.toStringTag] === 'BigUint64Array'
+  );
 }
 
 export function isRegExp(d: unknown): d is RegExp {
   return Object.prototype.toString.call(d) === '[object RegExp]';
 }
 
-export function isMap(d: unknown): d is Map<unknown, unknown> {
-  return Object.prototype.toString.call(d) === '[object Map]';
+export function isMap(value: unknown): d is Map<unknown, unknown> {
+  return (
+    typeof value === 'object' &&
+    value != null &&
+    Symbol.toStringTag in value &&
+    value[Symbol.toStringTag] === 'Map'
+  );
 }
 
 export function isDate(d: unknown): d is Date {
