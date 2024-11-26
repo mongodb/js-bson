fac472e10bf6d70a8c147219aae0749c16d0337c
Neal Beeken <neal.beeken@mongodb.com>
2024-11-26 15:41:01 -0500

    chore: comment for reference

--- src/parser/utils.ts
+++ src/parser/utils.ts
@@ -1,4 +1,6 @@
 const TypedArrayPrototypeGetSymbolToStringTag = (() => {
+  // Type check system lovingly referenced from:
+  // https://github.com/nodejs/node/blob/7450332339ed40481f470df2a3014e2ec355d8d8/lib/internal/util/types.js#L13-L15
   // eslint-disable-next-line @typescript-eslint/unbound-method -- the intention is to call this method with a bound value
   const g = Object.getOwnPropertyDescriptor(
     Object.getPrototypeOf(Uint8Array.prototype),
