ef4e52a5d817a0fc5685ba6499b94c2fb41eb16d
Durran Jordan <durran@gmail.com>
2025-01-07 15:35:28 +0100

    chore(NODE-6634): pin NPM to 10 when Node version is 18

--- .evergreen/install-dependencies.sh
+++ .evergreen/install-dependencies.sh
@@ -7,9 +7,8 @@ set -o errexit  # Exit the script with error if any of the commands fail
 ## a full nodejs version, in the format v<major>.<minor>.patch
 export NODE_LTS_VERSION=${NODE_LTS_VERSION:-16}
 # npm version can be defined in the environment for cases where we need to install
-# a version lower than latest to support EOL Node versions.
-export NPM_VERSION=${NPM_VERSION:-latest}
-
+# a version lower than latest to support EOL Node versions. When not provided will
+# be handled by this script in drivers tools.
 source $DRIVERS_TOOLS/.evergreen/install-node.sh
 
 npm install "${NPM_OPTIONS}"
