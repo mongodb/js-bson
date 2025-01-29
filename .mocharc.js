972d67d1a98d19a54e1e1170f5772e5137f4cf99
Warren James <warren.james@mongodb.com>
2025-01-29 14:46:08 -0500

    review comments

--- .mocharc.js
+++ .mocharc.js
@@ -1,27 +1,18 @@
 'use strict';
 
-
-const nodeOptions = ["experimental-vm-modules"];
-const [major, _minor, _patch] = process.versions.node.split('.');
+const nodeOptions = ['experimental-vm-modules'];
+const [major] = process.versions.node.split('.');
 if (Number(major) >= 23) {
   nodeOptions.push('no-experimental-strip-types');
 }
 
-module.exports =
-{
-  $schema: "https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/schemas/json/mocharc.json",
-  require: [
-    "source-map-support/register",
-    "ts-node/register"
-  ],
-  extension: [
-    "js",
-    "ts"
-  ],
+module.exports = {
+  require: ['source-map-support/register', 'ts-node/register'],
+  extension: ['js', 'ts'],
   recursive: true,
   timeout: 10000,
   failZero: true,
   sort: true,
   color: true,
-  "node-option": nodeOptions
-}
+  'node-option': nodeOptions
+};
