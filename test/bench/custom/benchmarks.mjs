54730a666c4a52409f6ef2b8af313fc4e4c7a3d3
Warren James <warren.james@mongodb.com>
2025-02-27 14:15:44 -0500

    add normalized measurements to custom benchmarks

--- test/bench/custom/benchmarks.mjs
+++ test/bench/custom/benchmarks.mjs
@@ -1,22 +1,66 @@
 /* eslint-disable strict */
 import { BSON } from '../../../lib/bson.mjs';
 
-const ObjectId_isValid = [
-  function objectid_isvalid_wrong_string_length() {
-    BSON.ObjectId.isValid('a');
-  },
-  /** wrong character at the start, could be the most short circuited code path */
-  function objectid_isvalid_invalid_hex_at_start() {
-    BSON.ObjectId.isValid('g6e84ebdc96f4c0772f0cbbf');
-  },
-  /** wrong character at the end, could be the least short circuited code path */
-  function objectid_isvalid_invalid_hex_at_end() {
-    BSON.ObjectId.isValid('66e84ebdc96f4c0772f0cbbg');
-  },
-  function objectid_isvalid_valid_hex_string() {
-    BSON.ObjectId.isValid('66e84ebdc96f4c0772f0cbbf');
+const ObjectId_isValid = {
+  name: 'ObjectId_isValid',
+  tags: ['alerting-benchmark', 'objectid'],
+  benchmarks: [
+    function objectid_isvalid_wrong_string_length() {
+      BSON.ObjectId.isValid('a');
+    },
+    /** wrong character at the start, could be the most short circuited code path */
+    function objectid_isvalid_invalid_hex_at_start() {
+      BSON.ObjectId.isValid('g6e84ebdc96f4c0772f0cbbf');
+    },
+    /** wrong character at the end, could be the least short circuited code path */
+    function objectid_isvalid_invalid_hex_at_end() {
+      BSON.ObjectId.isValid('66e84ebdc96f4c0772f0cbbg');
+    },
+    function objectid_isvalid_valid_hex_string() {
+      BSON.ObjectId.isValid('66e84ebdc96f4c0772f0cbbf');
+    }
+  ]
+};
+
+function sieveOfEratosthenes(n) {
+  // Create a boolean array "prime[0..n]" and initialize
+  // all entries as true. A value in prime[i] will
+  // become false if i is Not a prime
+  const prime = Array.from({ length: n + 1 }, () => true);
+
+  // We know 0 and 1 are not prime
+  prime[0] = false;
+  prime[1] = false;
+
+  for (let p = 2; p * p <= n; p++) {
+    // If prime[p] is not changed, then it is a prime
+    if (prime[p] === true) {
+      // Update all multiples of p as false
+      for (let i = p * p; i <= n; i += p) {
+        prime[i] = false;
+      }
+    }
   }
-];
+
+  // Collecting all prime numbers
+  const primes = [];
+  for (let i = 2; i <= n; i++) {
+    if (prime[i] === true) {
+      primes.push(i);
+    }
+  }
+
+  return primes;
+}
+const CPUBaseline = {
+  name: 'CPUBaseline',
+  tags: [],
+  benchmarks: [
+    function cpuBaseline() {
+      sieveOfEratosthenes(1_000_000);
+    }
+  ]
+};
 
 // Add benchmarks here:
-export const benchmarks = [...ObjectId_isValid];
+export const suites = [ObjectId_isValid, CPUBaseline];
--- test/bench/custom/main.mjs
+++ test/bench/custom/main.mjs
@@ -4,7 +4,7 @@ import util from 'node:util';
 import fs from 'node:fs';
 import os from 'node:os';
 import benchmark from 'benchmark';
-import { benchmarks } from './benchmarks.mjs';
+import { suites } from './benchmarks.mjs';
 
 const hw = os.cpus();
 const ram = os.totalmem() / 1024 ** 3;
@@ -20,20 +20,66 @@ const systemInfo = () =>
   ].join('\n');
 console.log(systemInfo());
 
-const suite = new benchmark.Suite();
+function logBenchmark(event) {
+  console.log(String(event.target));
+}
 
-for (const bench of benchmarks) suite.add(bench.name, bench);
+function processBenchmarkResult(bench, tags, metadata) {
+  return {
+    info: { test_name: bench.name },
+    metrics: [{ name: 'ops_per_sec', value: bench.hz, metadata }],
+    tags
+  };
+}
 
-suite
-  .on('cycle', function logBenchmark(event) {
-    console.log(String(event.target));
-  })
-  .on('complete', function outputPerfSend() {
-    const data = Array.from(this).map(bench => ({
-      info: { test_name: bench.name },
-      metrics: [{ name: 'ops_per_sec', value: bench.hz }]
-    }));
-    console.log(util.inspect(data, { depth: Infinity, colors: true }));
-    fs.writeFileSync('customBenchmarkResults.json', JSON.stringify(data), 'utf8');
-  })
-  .run();
+let completedSuites = 0;
+function completeSuite() {
+  const metadata = { improvement_direction: 'up' };
+  if (++completedSuites >= collectedSuites.length) {
+    const data = [];
+    const cpuBaseline = collectedSuites.find(d => d.suite.name === 'CPUBaseline');
+    if (!cpuBaseline) throw new Error("couldn't find baseline!");
+
+    const cpuBaselineResult = cpuBaseline.suite[0].hz;
+    if (typeof cpuBaselineResult !== 'number') {
+      throw new Error("Couldn't find baseline result");
+    }
+
+    data.push(processBenchmarkResult(cpuBaseline.suite[0], cpuBaseline.suiteConfig.tags, metadata));
+
+    for (const { suite, suiteConfig } of collectedSuites) {
+      const { name, tags } = suiteConfig;
+      if (name === 'CPUBaseline') continue;
+
+      for (const bench of Array.from(suite)) {
+        const result = processBenchmarkResult(bench, tags, metadata);
+        result.metrics.push({
+          name: 'normalized_throughput',
+          value: bench.hz / cpuBaselineResult,
+          metadata
+        });
+        data.push(result);
+      }
+
+      console.log(util.inspect(data, { depth: Infinity, colors: true }));
+      fs.writeFileSync('customBenchmarkResults.json', JSON.stringify(data), 'utf8');
+    }
+  }
+}
+
+function processSuite(suiteModule, cycleHandler, completeHandler) {
+  let suite = new benchmark.Suite(suiteModule.name);
+  for (const b of suiteModule.benchmarks) {
+    suite.add(b.name, b);
+  }
+
+  suite = suite.on('cycle', cycleHandler).on('complete', completeHandler).run({ async: true });
+
+  return { suite, suiteConfig: suiteModule };
+}
+
+const collectedSuites = [];
+for (const suite of suites) {
+  const newSuite = processSuite(suite, logBenchmark, completeSuite);
+  collectedSuites.push(newSuite);
+}
