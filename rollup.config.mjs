46029739ace8596beb5c397eae7992b23c5872f4
Louis DeScioli <louis.descioli@gmail.com>
2025-04-24 15:37:04 -0400

    fix(NODE-6074): Removes top-level await in bson with separate node and browser ESM bundles (#749)

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import { RequireRewriter } from './etc/rollup/rollup-plugin-require-rewriter/require_rewriter.mjs';
+import { requireRewriter } from './etc/rollup/rollup-plugin-require-rewriter/require_rewriter.mjs';
 import { RequireVendor } from './etc/rollup/rollup-plugin-require-vendor/require_vendor.mjs';
 
 /** @type {typescript.RollupTypescriptOptions} */
@@ -55,9 +55,22 @@ const config = [
   },
   {
     input,
-    plugins: [typescript(tsConfig), new RequireRewriter(), nodeResolve({ resolveOnly: [] })],
+    plugins: [
+      typescript(tsConfig),
+      requireRewriter({ isBrowser: true }),
+      nodeResolve({ resolveOnly: [] })
+    ],
     output: {
-      file: 'lib/bson.mjs',
+      file: 'lib/bson.browser.mjs',
+      format: 'esm',
+      sourcemap: true
+    }
+  },
+  {
+    input,
+    plugins: [typescript(tsConfig), requireRewriter(), nodeResolve({ resolveOnly: [] })],
+    output: {
+      file: 'lib/bson.node.mjs',
       format: 'esm',
       sourcemap: true
     }
