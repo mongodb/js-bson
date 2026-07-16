57b5bf07d9fe734e6dabd1972414abc8ffaed2c8
Pavel Safronov <pavel.safronov@gmail.com>
2026-07-16 05:05:20 -0700

    chore: use bundled version of NPM (#913)

--- .evergreen/install-dependencies.sh
+++ .evergreen/install-dependencies.sh
@@ -6,9 +6,13 @@ set -o errexit  # Exit the script with error if any of the commands fail
 ## 'latest'
 ## a full nodejs version, in the format v<major>.<minor>.patch
 export NODE_LTS_VERSION=${NODE_LTS_VERSION:-20.19.0}
-# npm version can be defined in the environment for cases where we need to install
-# a version lower than latest to support EOL Node versions. When not provided will
-# be handled by this script in drivers tools.
+# Use the npm bundled with the installed Node.js release instead of upgrading to
+# npm@latest. The bundled npm is version-matched to the Node release; upgrading to
+# npm@latest (currently 12) breaks `npm install` of git dependencies (EALLOWGIT).
+# TODO(NODE-7677): revisit disabling the npm upgrade once our git dependency
+# (dbx-js-tools) can be installed under npm 12 — e.g. by publishing bson-bench to a
+# registry — at which point we can drop this flag and test on npm@latest again.
+export SKIP_NPM_UPGRADE=true
 source $DRIVERS_TOOLS/.evergreen/install-node.sh
 
 npm install "${NPM_OPTIONS}"
