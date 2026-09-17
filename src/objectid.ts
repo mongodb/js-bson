4d412876b0324864c92f81a8559dee6b8b4df5b8
Johnathan Martell <286711055+johnmtll@users.noreply.github.com>
2026-09-17 17:09:04 -0400

    docs: issue callouts to support maintenance efforts

--- src/objectid.ts
+++ src/objectid.ts
@@ -62,9 +62,20 @@ export class ObjectId extends BSONValue {
     let startupSnapshot;
     let isBuildingSnapshot = false;
     try {
+      // NODE-7784: getBuiltInModule is wrapped in a try..catch due to 
+      // https://github.com/vercel/next.js/issues/98226.
+      //
+      // The NextJS Edge runtime
+      // implements getBuiltInModule with a throwable stub, which we protect
+      // against so our users are operable across this popular runtime.
       // @ts-expect-error Node.js types not present since this is an optional API
       const v8Module = globalThis?.process?.getBuiltinModule?.('v8') ?? {};
       startupSnapshot = v8Module?.startupSnapshot;
+      // NODE-7784: isBuildingSnapshot is wrapped in a try..catch due to
+      // https://github.com/oven-sh/bun/issues/32501
+      //
+      // Bun implements isBuildingSnapshot with a throwable stub, which we also
+      // protect against for the same reason as getBuiltInModule.
       isBuildingSnapshot = startupSnapshot?.isBuildingSnapshot?.();
     } catch {
       // Suppress
